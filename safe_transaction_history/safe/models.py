25b2d57fca1e7cffdc3af5d8680c10dfa9a4f66d
Uxio Fuentefria <uxio@gnosis.pm>
2018-08-10 11:21:40 +0200

    Fix fields for ethereum uint256

--- safe_transaction_history/safe/models.py
+++ safe_transaction_history/safe/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django_eth.models import EthereumAddressField
+from django_eth.models import EthereumAddressField, Uint256Field
 from model_utils.models import TimeStampedModel
 
 
 class MultisigTransaction(TimeStampedModel):
     safe = EthereumAddressField()
     to = EthereumAddressField()
-    value = models.BigIntegerField()
+    value = Uint256Field()
     data = models.BinaryField(null=True)
     operation = models.PositiveSmallIntegerField()
-    nonce = models.BigIntegerField()
+    nonce = Uint256Field()
     status = models.BooleanField(default=False)  # True if transaction executed, 0 otherwise
     # Defines when a multisig transaction gets executed (confirmations included)
     execution_date = models.DateTimeField(blank=True, null=True)
@@ -23,11 +23,9 @@ class MultisigConfirmation(TimeStampedModel):
     contract_transaction_hash = models.CharField(max_length=66, null=False, blank=False)
     transaction_hash = models.CharField(max_length=66, null=False, blank=False)
     type = models.CharField(max_length=20, null=False, blank=False)
-    block_number = models.IntegerField()
+    block_number = models.BigIntegerField()
     block_date_time = models.DateTimeField()
-    status = models.BooleanField(
-        default=False
-    )  # True if transaction mined and executed successfully, 0 otherwise
+    status = models.BooleanField(default=False)  # True if transaction mined and executed successfully, 0 otherwise
     multisig_transaction = models.ForeignKey(MultisigTransaction,
                                              on_delete=models.CASCADE,
                                              related_name="confirmations")
