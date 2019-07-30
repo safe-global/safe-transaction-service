d7cec2dda1994d84059683cb1e7b48221797930c
Uxio Fuentefria <uxio@gnosis.pm>
2019-07-30 13:07:33 +0200

    Fix typo

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from drf_yasg.views import get_schema_view
 
 schema_view = get_schema_view(
     openapi.Info(
-        title='Gnosis Safe Transaction History API',
+        title='Gnosis Safe Transaction Service API',
         default_version='v1',
         description='API to store safe multisig transactions',
         contact=openapi.Contact(email='dev@gnosis.pm'),
