a1447a72d9df115e9a6584efe31539e527fcec43
Uxio Fuentefria <uxio@gnosis.pm>
2019-08-02 16:37:36 +0200

    Use Redis

--- safe_transaction_service/history/tasks.py
+++ safe_transaction_service/history/tasks.py
@@ -3,6 +3,7 @@ from celery.utils.log import get_task_logger
 from django.conf import settings
 from gnosis.eth import EthereumClientProvider
 from gnosis.safe import Safe
+from redis import Redis
 
 from .models import MultisigConfirmation
 from .services.proxy_factory_indexer import ProxyIndexerServiceProvider
@@ -90,12 +91,17 @@ def index_new_proxies(self) -> int:
     :return: Number of proxies created
     """
 
-    proxy_factory_addresses = ['0x12302fE9c02ff50939BaAaaf415fc226C078613C']
-    proxy_indexer_service = ProxyIndexerServiceProvider()
+    redis = Redis.from_url(settings.REDIS_URL)
+    with redis.lock('tasks:index_new_proxies', blocking_timeout=1, timeout=60 * 30):
+        proxy_factory_addresses = ['0x12302fE9c02ff50939BaAaaf415fc226C078613C']
+        proxy_indexer_service = ProxyIndexerServiceProvider()
 
-    new_monitored_addresses = 0
-    updated = False
-    while not updated:
-        created_objects, updated = proxy_indexer_service.process_addresses(proxy_factory_addresses)
-        new_monitored_addresses += len(created_objects)
+        new_monitored_addresses = 0
+        updated = False
+
+        while not updated:
+            created_objects, updated = proxy_indexer_service.process_addresses(proxy_factory_addresses)
+            new_monitored_addresses += len(created_objects)
+        logger.info('Indexed new %d proxies', new_monitored_addresses)
+
     return new_monitored_addresses
