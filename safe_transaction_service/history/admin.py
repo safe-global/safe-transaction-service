21d09f7d5883b01ab62a7606f9705d2f8df55caa
Uxio Fuentefria <uxio@gnosis.pm>
2019-08-13 10:48:59 +0200

    Add block_number to Internal Tx decoded

--- safe_transaction_service/history/admin.py
+++ safe_transaction_service/history/admin.py
@@ -49,7 +49,7 @@ class InternalTxAdmin(admin.ModelAdmin):
 
 @admin.register(InternalTxDecoded)
 class InternalTxDecodedAdmin(admin.ModelAdmin):
-    list_display = ('internal_tx_id', 'processed', 'function_name', 'arguments')
+    list_display = ('block_number', 'internal_tx_id', 'processed', 'function_name', 'arguments')
     list_filter = ('function_name', 'processed')
     list_select_related = ('internal_tx__ethereum_tx',)
     search_fields = ['function_name', 'arguments']
