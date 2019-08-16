0d6e3a300b2f90613f02c2537b656051b8565b63
Uxio Fuentefria <uxio@gnosis.pm>
2019-08-16 17:02:00 +0200

    Bind confirmations

---
+++ safe_transaction_service/history/management/commands/bind_confirmations.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from eth_account import Account
+
+from gnosis.eth import EthereumClientProvider
+from gnosis.safe import Safe, ProxyFactory
+from ...models import MultisigConfirmation, MultisigTransaction
+
+
+class Command(BaseCommand):
+    help = 'Binds confirmations with multisig txs'
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        # parser.add_argument('--deployer-key', help='Private key for deployer')
+        pass
+
+    def handle(self, *args, **options):
+        for multisig_confirmation in MultisigConfirmation.objects.without_transaction():
+            try:
+                tx = MultisigTransaction.objects.get(safe_tx_hash=multisig_confirmation.multisig_transaction_hash)
+                multisig_confirmation.multisig_transaction = tx
+                multisig_confirmation.save(update_fields=['multisig_transaction'])
+                self.stdout.write(self.style.SUCCESS(f'Bind confirmation with multisig tx={tx.safe_tx_hash}'))
+            except MultisigTransaction.DoesNotExist:
+                pass
+
