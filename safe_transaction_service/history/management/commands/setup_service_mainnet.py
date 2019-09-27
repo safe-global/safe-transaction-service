93888028df64f7db7e3e09cdf504c1e6be184792
Uxio Fuentefria <uxio@gnosis.pm>
2019-09-27 11:48:33 +0200

    Fix typos

--- safe_transaction_service/history/management/commands/setup_service_mainnet.py
+++ safe_transaction_service/history/management/commands/setup_service_mainnet.py
@@ -5,17 +5,17 @@ from ...models import SafeMasterCopy, ProxyFactory
 
 
 class Command(BaseCommand):
-    help = 'Setup Transaction Service For Rinkeby'
+    help = 'Setup Transaction Service For Mainnet'
 
     def handle(self, *args, **options):
         call_command('setup_service')
         SafeMasterCopy.objects.get_or_create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
                                              defaults={
-                                                 'initial_block_number': 4110083,
-                                                 'tx_block_number': 4110083,
+                                                 'initial_block_number': 7457553,
+                                                 'tx_block_number': 7457553,
                                              })
         ProxyFactory.objects.get_or_create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
                                            defaults={
-                                               'initial_block_number': 4110083,
-                                               'tx_block_number': 4110083,
+                                               'initial_block_number': 7450116,
+                                               'tx_block_number': 7450116,
                                            })
