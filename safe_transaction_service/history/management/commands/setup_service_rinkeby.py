db899622dde3ee5602a480bb64cdd443d40ab084
Uxio Fuentefria <uxio@gnosis.pm>
2019-09-27 11:39:19 +0200

    Add setup commands for rinkeby/mainnet

---
+++ safe_transaction_service/history/management/commands/setup_service_rinkeby.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+from ...models import SafeMasterCopy, ProxyFactory
+
+
+class Command(BaseCommand):
+    help = 'Setup Transaction Service For Rinkeby'
+
+    def handle(self, *args, **options):
+        call_command('setup_service')
+        SafeMasterCopy.objects.create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
+                                      initial_block_number=4110083,
+                                      tx_block_number=4110083)
+        ProxyFactory.objects.create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
+                                    initial_block_number=4110083,
+                                    tx_block_number=4110083)
