e8788bd216e843b434d07aecc526d9c5904ef1e1
Uxio Fuentefria <uxio@gnosis.pm>
2019-10-04 11:51:05 +0200

    Use less workers

--- safe_transaction_service/history/indexers/internal_tx_indexer.py
+++ safe_transaction_service/history/indexers/internal_tx_indexer.py
@@ -72,7 +72,6 @@ class InternalTxIndexer(TransactionIndexer):
         # TODO Remove from here. Prefetch txs
         if transaction_hashes:
             logger.info('Prefetching txs')
-            self.cached_ethereum_txs = {}
             for ethereum_tx in EthereumTx.objects.create_or_update_from_tx_hashes(transaction_hashes):
                 self.cached_ethereum_txs[ethereum_tx.tx_hash] = ethereum_tx
             logger.info('End prefetching of txs')
@@ -91,12 +90,12 @@ class InternalTxIndexer(TransactionIndexer):
         logger.info('Got traces %d for tx-hash=%s', len(traces), tx_hash)
         logger.info('Fetching ethereum tx with tx-hash=%s', tx_hash)
         # ethereum_tx = EthereumTx.objects.create_or_update_from_tx_hash(tx_hash)
-        ethereum_tx = self.cached_ethereum_txs[tx_hash]
+        ethereum_tx = self.cached_ethereum_txs.pop(tx_hash)
         logger.info('Got ethereum tx with tx-hash=%s', tx_hash)
 
         # return [self._process_trace(trace, ethereum_tx) for trace in traces]
         # Use multiprocessing to process traces in parallel
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=6) as executor:
             future_internal_txs = [executor.submit(self._process_trace, trace, ethereum_tx)
                                    for trace in traces]
 
