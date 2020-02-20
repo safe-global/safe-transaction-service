0b0fa9be401f5151f2e438c6464945fff42b677d
Uxio Fuentefria <uxio@gnosis.pm>
2020-02-20 10:29:58 +0100

    Revert "Update Celery configuration for v4.0.0"
    This reverts commit f2c8fd8d327afcd3e565e61bc3c426398d3eb961.

--- config/settings/local.py
+++ config/settings/local.py
@@ -54,7 +54,7 @@ INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 # Celery
 # ------------------------------------------------------------------------------
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_always_eager
-CELERY_TASK_ALWAYS_EAGER = False
+CELERY_ALWAYS_EAGER = False
 
 if env.bool('USE_DOCKER', default=False):
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
