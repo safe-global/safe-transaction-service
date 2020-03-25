90b61bdbad1ad50851aeec47d2eb9e2207d01024
Uxio Fuentefria <uxio@gnosis.pm>
2020-03-25 17:58:31 +0100

    Add context manager for blockchain tasks

--- safe_transaction_service/history/tests/test_tasks.py
+++ safe_transaction_service/history/tests/test_tasks.py
@@ -5,8 +5,8 @@ from django.test import TestCase
 
 from eth_account import Account
 
-from safe_transaction_service.history.tests.factories import (
-    EthereumEventFactory, InternalTxFactory, WebHookFactory)
+from ..tasks import BlockchainRunningTask, BlockchainRunningTaskManager
+from .factories import EthereumEventFactory, InternalTxFactory, WebHookFactory
 
 logger = logging.getLogger(__name__)
 
@@ -25,3 +25,21 @@ class TestTasks(TestCase):
         WebHookFactory(address=to)
         InternalTxFactory(to=to)
         self.assertEqual(mock_post.call_count, 2)
+
+    def test_blockchain_running_task(self):
+        # Test context manager
+        class A:
+            def __init__(self, id: str):
+                self.id = id
+
+        BlockchainRunningTaskManager().delete_all_tasks()
+        a = A('custom-task-id')
+        b = A('another-task_id')
+        with BlockchainRunningTask(a) as blockchain_running_task:
+            self.assertEqual(blockchain_running_task.blockchain_running_task_manager.get_running_tasks(),
+                             [a.id])
+            with BlockchainRunningTask(b):
+                self.assertEqual(blockchain_running_task.blockchain_running_task_manager.get_running_tasks(),
+                                 [b.id, a.id])
+
+        self.assertEqual(BlockchainRunningTaskManager().get_running_tasks(), [])
