8c17c5a30e2cf967fbe5b0d08d8038c6fecc9305
Uxio Fuentefria <uxio@gnosis.pm>
2020-04-08 17:17:17 +0200

    Add verbose app name

--- safe_transaction_service/history/apps.py
+++ safe_transaction_service/history/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class HistoryConfig(AppConfig):
     name = 'safe_transaction_service.history'
+    verbose_name = 'Safe Transaction Service'
 
     def ready(self):
         from . import signals  # noqa
