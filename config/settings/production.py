8e6af734d2dbe65b6c8b90666a46b2e2f8965362
Uxio Fuentefria <uxio@gnosis.pm>
2020-05-13 14:22:45 +0200

    Revert "Enable DEBUG toolbar"
    This reverts commit 99cdbabb29ab00bd0ee2878fec2f54690a060e70.

--- config/settings/production.py
+++ config/settings/production.py
@@ -90,17 +90,3 @@ ADMIN_URL = env('DJANGO_ADMIN_URL', default=r'^admin/')
 # Gunicorn
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ['gunicorn']  # noqa F405
-
-INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
-DEBUG = True
-INSTALLED_APPS += ['debug_toolbar']  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',
-               'debug_toolbar_force.middleware.ForceDebugToolbarMiddleware']  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-DEBUG_TOOLBAR_CONFIG = {
-    'DISABLE_PANELS': [
-        'debug_toolbar.panels.redirects.RedirectsPanel',
-    ],
-    'SHOW_TEMPLATE_CONTEXT': True,
-}
--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
 from django.views import defaults as default_views
 from django.views.decorators.cache import cache_control
 
@@ -64,6 +63,6 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
 
 admin.autodiscover()
