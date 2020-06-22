f8c5798e0a1e7414d657bdde82195daa2f90bfb7
Uxio Fuentefria <uxio@gnosis.pm>
2020-06-22 14:14:25 +0200

    Add trusted to token admin

--- safe_transaction_service/tokens/admin.py
+++ safe_transaction_service/tokens/admin.py
@@ -5,7 +5,7 @@ from .models import Token
 
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ('address', 'name', 'symbol', 'decimals')
+    list_display = ('address', 'trusted', 'name', 'symbol', 'decimals')
     list_filter = ('decimals', 'trusted')
     ordering = ('name',)
     search_fields = ['symbol', 'address', 'name']
