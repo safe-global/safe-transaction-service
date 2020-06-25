0c5e7c0ac807d3a5c3cbb2750e0c2ceaabdd205e
Uxio Fuentefria <uxio@gnosis.pm>
2020-06-25 15:55:32 +0200

    Support version v1.2.0 of the Safe contracts

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Tuple
+from typing import NamedTuple, Sequence, Tuple
 
 from django.core.management.base import BaseCommand
 
@@ -57,178 +57,106 @@ class Command(BaseCommand):
         ethereum_client = EthereumClientProvider()
         ethereum_network = ethereum_client.get_network()
         if ethereum_network == EthereumNetwork.MAINNET:
-            self.stdout.write(self.style.SUCCESS('Setting up Mainnet addresses'))
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} addresses'))
             self.setup_mainnet()
         elif ethereum_network == EthereumNetwork.RINKEBY:
-            self.stdout.write(self.style.SUCCESS('Setting up Rinkeby addresses'))
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} addresses'))
             self.setup_rinkeby()
         elif ethereum_network == EthereumNetwork.GOERLI:
-            self.stdout.write(self.style.SUCCESS('Setting up Goerli addresses'))
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} addresses'))
             self.setup_goerli()
         elif ethereum_network == EthereumNetwork.KOVAN:
-            self.stdout.write(self.style.SUCCESS('Setting up Kovan addresses'))
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} addresses'))
             self.setup_kovan()
         else:
-            self.stdout.write(self.style.WARNING(f'Cannot detect a valid ethereum-network'))
+            self.stdout.write(self.style.WARNING('Cannot detect a valid ethereum-network'))
+
+    def _setup_safe_master_copies(self, safe_master_copies: Sequence[Tuple[str, int]]):
+        for address, initial_block_number in safe_master_copies:
+            SafeMasterCopy.objects.get_or_create(address=address,
+                                                 defaults={
+                                                     'initial_block_number': initial_block_number,
+                                                     'tx_block_number': initial_block_number,
+                                                 })
+
+    def _setup_safe_proxy_factories(self, safe_proxy_factories: Sequence[Tuple[str, int]]):
+        for address, initial_block_number in safe_proxy_factories:
+            ProxyFactory.objects.get_or_create(address=address,
+                                               defaults={
+                                                   'initial_block_number': initial_block_number,
+                                                   'tx_block_number': initial_block_number,
+                                               })
 
     def setup_mainnet(self):
-        SafeMasterCopy.objects.get_or_create(address='0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
-                                             defaults={
-                                                 'initial_block_number': 9084503,
-                                                 'tx_block_number': 9084503,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xaE32496491b53841efb51829d6f886387708F99B',
-                                             defaults={
-                                                 'initial_block_number': 8915728,
-                                                 'tx_block_number': 8915728,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
-                                             defaults={
-                                                 'initial_block_number': 7457553,
-                                                 'tx_block_number': 7457553,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0x8942595A2dC5181Df0465AF0D7be08c8f23C93af',
-                                             defaults={
-                                                 'initial_block_number': 6766257,
-                                                 'tx_block_number': 6766257,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xAC6072986E985aaBE7804695EC2d8970Cf7541A2',
-                                             defaults={
-                                                 'initial_block_number': 6569433,
-                                                 'tx_block_number': 6569433,
-                                             })
+        safe_master_copies = [
+            ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10329734),  # v1.2.0
+            ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 9084503),   # v1.1.1
+            ('0xaE32496491b53841efb51829d6f886387708F99B', 8915728),   # v1.1.0
+            ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 7457553),   # v1.0.0
+            ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 6766257),   # v0.1.0
+            ('0xAC6072986E985aaBE7804695EC2d8970Cf7541A2', 6569433),   # v0.0.2
+        ]
 
-        ProxyFactory.objects.get_or_create(address='0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
-                                           defaults={
-                                               'initial_block_number': 9084508,
-                                               'tx_block_number': 9084508,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x50e55Af101C777bA7A1d560a774A82eF002ced9F',
-                                           defaults={
-                                               'initial_block_number': 8915731,
-                                               'tx_block_number': 8915731,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
-                                           defaults={
-                                               'initial_block_number': 7450116,
-                                               'tx_block_number': 7450116,
-                                           })
+        safe_proxy_factories = [
+            ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 9084508),
+            ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 8915731),
+            ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 7450116),
+        ]
+
+        self._setup_safe_master_copies(safe_master_copies)
+        self._setup_safe_proxy_factories(safe_proxy_factories)
 
     def setup_rinkeby(self):
-        SafeMasterCopy.objects.get_or_create(address='0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
-                                             defaults={
-                                                 'initial_block_number': 5590754,
-                                                 'tx_block_number': 5590754,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xaE32496491b53841efb51829d6f886387708F99B',
-                                             defaults={
-                                                 'initial_block_number': 5423491,
-                                                 'tx_block_number': 5423491,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
-                                             defaults={
-                                                 'initial_block_number': 4110083,
-                                                 'tx_block_number': 4110083,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0x8942595A2dC5181Df0465AF0D7be08c8f23C93af',
-                                             defaults={
-                                                 'initial_block_number': 3392692,
-                                                 'tx_block_number': 3392692,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7',
-                                             defaults={
-                                                 'initial_block_number': 3055781,
-                                                 'tx_block_number': 3055781,
-                                             })
+        safe_master_copies = [
+            ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6723632),  # v1.2.0
+            ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 5590754),  # v1.1.1
+            ('0xaE32496491b53841efb51829d6f886387708F99B', 5423491),  # v1.1.0
+            ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 4110083),  # v1.0.0
+            ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 3392692),  # v0.1.0
+            ('0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7', 3055781),  # v0.0.2
+        ]
 
-        ProxyFactory.objects.get_or_create(address='0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
-                                           defaults={
-                                               'initial_block_number': 5590757,
-                                               'tx_block_number': 5590757,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x50e55Af101C777bA7A1d560a774A82eF002ced9F',
-                                           defaults={
-                                               'initial_block_number': 5423494,
-                                               'tx_block_number': 5423494,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
-                                           defaults={
-                                               'initial_block_number': 4110083,
-                                               'tx_block_number': 4110083,
-                                           })
+        safe_proxy_factories = [
+            ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 5590757),
+            ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 5423494),
+            ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 4110083),
+        ]
+
+        self._setup_safe_master_copies(safe_master_copies)
+        self._setup_safe_proxy_factories(safe_proxy_factories)
 
     def setup_goerli(self):
-        SafeMasterCopy.objects.get_or_create(address='0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
-                                             defaults={
-                                                 'initial_block_number': 1798663,
-                                                 'tx_block_number': 1798663,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xaE32496491b53841efb51829d6f886387708F99B',
-                                             defaults={
-                                                 'initial_block_number': 1631488,
-                                                 'tx_block_number': 1631488,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
-                                             defaults={
-                                                 'initial_block_number': 319108,
-                                                 'tx_block_number': 319108,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0x8942595A2dC5181Df0465AF0D7be08c8f23C93af',
-                                             defaults={
-                                                 'initial_block_number': 3392692,
-                                                 'tx_block_number': 3392692,
-                                             })
+        safe_master_copies = [
+            ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 2930373),  # v1.2.0
+            ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 1798663),  # v1.1.1
+            ('0xaE32496491b53841efb51829d6f886387708F99B', 1631488),  # v1.1.0
+            ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 319108),   # v1.0.0
+            ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 3392692),  # v0.1.0
+        ]
 
-        ProxyFactory.objects.get_or_create(address='0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
-                                           defaults={
-                                               'initial_block_number': 1798666,
-                                               'tx_block_number': 1798666,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x50e55Af101C777bA7A1d560a774A82eF002ced9F',
-                                           defaults={
-                                               'initial_block_number': 1631491,
-                                               'tx_block_number': 1631491,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
-                                           defaults={
-                                               'initial_block_number': 312509,
-                                               'tx_block_number': 312509,
-                                           })
+        safe_proxy_factories = [
+            ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 1798666),
+            ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 1631491),
+            ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 312509),
+        ]
+
+        self._setup_safe_master_copies(safe_master_copies)
+        self._setup_safe_proxy_factories(safe_proxy_factories)
 
     def setup_kovan(self):
-        SafeMasterCopy.objects.get_or_create(address='0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F',
-                                             defaults={
-                                                 'initial_block_number': 15366145,
-                                                 'tx_block_number': 15366145,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xaE32496491b53841efb51829d6f886387708F99B',
-                                             defaults={
-                                                 'initial_block_number': 14740724,
-                                                 'tx_block_number': 14740724,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A',
-                                             defaults={
-                                                 'initial_block_number': 10638132,
-                                                 'tx_block_number': 10638132,
-                                             })
-        SafeMasterCopy.objects.get_or_create(address='0x8942595A2dC5181Df0465AF0D7be08c8f23C93af',
-                                             defaults={
-                                                 'initial_block_number': 9465686,
-                                                 'tx_block_number': 9465686,
-                                             })
+        safe_master_copies = [
+            ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 19242615),  # v1.2.0
+            ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 15366145),  # v1.1.1
+            ('0xaE32496491b53841efb51829d6f886387708F99B', 14740724),  # v1.1.0
+            ('0xb6029EA3B2c51D09a50B53CA8012FeEB05bDa35A', 10638132),  # v1.0.0
+            ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 9465686),   # v0.1.0
+        ]
 
-        ProxyFactory.objects.get_or_create(address='0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B',
-                                           defaults={
-                                               'initial_block_number': 15366151,
-                                               'tx_block_number': 15366151,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x50e55Af101C777bA7A1d560a774A82eF002ced9F',
-                                           defaults={
-                                               'initial_block_number': 14740731,
-                                               'tx_block_number': 14740731,
-                                           })
-        ProxyFactory.objects.get_or_create(address='0x12302fE9c02ff50939BaAaaf415fc226C078613C',
-                                           defaults={
-                                               'initial_block_number': 10629898,
-                                               'tx_block_number': 10629898,
-                                           })
+        safe_proxy_factories = [
+            ('0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B', 15366151),
+            ('0x50e55Af101C777bA7A1d560a774A82eF002ced9F', 14740731),
+            ('0x12302fE9c02ff50939BaAaaf415fc226C078613C', 10629898),
+        ]
+
+        self._setup_safe_master_copies(safe_master_copies)
+        self._setup_safe_proxy_factories(safe_proxy_factories)
---
+++ safe_transaction_service/history/tests/test_commands.py
@@ -0,0 +1,84 @@
+from io import StringIO
+from unittest import mock
+from unittest.mock import MagicMock
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from django_celery_beat.models import PeriodicTask
+
+from gnosis.eth.ethereum_client import EthereumClient, EthereumNetwork
+
+from ..models import ProxyFactory, SafeMasterCopy
+
+
+class TestCommands(TestCase):
+    @mock.patch.object(EthereumClient, 'get_network', autospec=True)
+    def _test_setup_service(self, ethereum_network: EthereumNetwork, ethereum_client_get_network_mock: MagicMock):
+        command = 'setup_service'
+        ethereum_client_get_network_mock.return_value = ethereum_network
+        buf = StringIO()
+        self.assertEqual(SafeMasterCopy.objects.count(), 0)
+        self.assertEqual(ProxyFactory.objects.count(), 0)
+        self.assertEqual(PeriodicTask.objects.count(), 0)
+
+        call_command(command, stdout=buf)
+        self.assertIn(f'Setting up {ethereum_network.name} addresses', buf.getvalue())
+        self.assertIn('Created Periodic Task', buf.getvalue())
+        self.assertNotIn('was already created', buf.getvalue())
+        self.assertGreater(SafeMasterCopy.objects.count(), 0)
+        self.assertGreater(ProxyFactory.objects.count(), 0)
+        self.assertGreater(PeriodicTask.objects.count(), 0)
+
+        # Check last master copy was created
+        last_master_copy_address = '0x6851D6fDFAfD08c0295C392436245E5bc78B0185'
+        last_master_copy = SafeMasterCopy.objects.get(address=last_master_copy_address)
+        self.assertGreater(last_master_copy.initial_block_number, 0)
+        self.assertGreater(last_master_copy.tx_block_number, 0)
+
+        # Check last proxy factory was created
+        last_proxy_factory_address = '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B'
+        last_proxy_factory = ProxyFactory.objects.get(address=last_proxy_factory_address)
+        self.assertGreater(last_proxy_factory.initial_block_number, 0)
+        self.assertGreater(last_proxy_factory.tx_block_number, 0)
+
+        buf = StringIO()
+        call_command(command, stdout=buf)
+        self.assertIn(f'Setting up {ethereum_network.name} addresses', buf.getvalue())
+        self.assertNotIn('Created Periodic Task', buf.getvalue())
+        self.assertIn('was already created', buf.getvalue())
+
+    def test_setup_service_mainnet(self):
+        self._test_setup_service(EthereumNetwork.MAINNET)
+
+        # Check last master copy was created
+        last_master_copy_address = '0x6851D6fDFAfD08c0295C392436245E5bc78B0185'
+        last_master_copy_initial_block = 10329734
+        last_master_copy = SafeMasterCopy.objects.get(address=last_master_copy_address)
+        self.assertEqual(last_master_copy.initial_block_number, last_master_copy_initial_block)
+        self.assertEqual(last_master_copy.tx_block_number, last_master_copy_initial_block)
+
+        # Check last proxy factory was created
+        last_proxy_factory_address = '0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B'
+        last_proxy_factory_initial_block = 9084508
+        last_proxy_factory = ProxyFactory.objects.get(address=last_proxy_factory_address)
+        self.assertEqual(last_proxy_factory.initial_block_number, last_proxy_factory_initial_block)
+        self.assertEqual(last_proxy_factory.tx_block_number, last_proxy_factory_initial_block)
+
+    def test_setup_service_rinkeby(self):
+        self._test_setup_service(EthereumNetwork.RINKEBY)
+
+    def test_setup_service_goerli(self):
+        self._test_setup_service(EthereumNetwork.GOERLI)
+
+    def test_setup_service_kovan(self):
+        self._test_setup_service(EthereumNetwork.KOVAN)
+
+    @mock.patch.object(EthereumClient, 'get_network', autospec=True)
+    def test_setup_service_not_valid_network(self, ethereum_client_get_network_mock: MagicMock):
+        command = 'setup_service'
+        for return_value in (EthereumNetwork.ROPSTEN, EthereumNetwork.UNKNOWN):
+            ethereum_client_get_network_mock.return_value = return_value
+            buf = StringIO()
+            call_command(command, stdout=buf)
+            self.assertIn('Cannot detect a valid ethereum-network', buf.getvalue())
