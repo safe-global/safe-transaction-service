78ee5f66fb1b522e94b95025fb124ad4d73bfec6
Uxio Fuentefria <uxio@gnosis.pm>
2020-07-23 14:39:43 +0200

    Add django check to run tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,5 @@ docker-compose -f docker-compose.yml -f docker-compose.dev.yml build --force-rm 
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --no-start db redis ganache
 docker restart safe-transaction-service_db_1 safe-transaction-service_redis_1 safe-transaction-service_ganache_1
 sleep 2
+DJANGO_SETTINGS_MODULE=config.settings.test DJANGO_DOT_ENV_FILE=.env_local python manage.py check
 DJANGO_SETTINGS_MODULE=config.settings.test DJANGO_DOT_ENV_FILE=.env_local pytest
