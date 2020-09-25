1fe24015c532032d468e1caf764a969f28b9144b
Uxio Fuentefria <uxio@gnosis.pm>
2020-09-25 14:47:16 +0200

    Fine tune auto block process limit on indexers

--- safe_transaction_service/history/indexers/ethereum_indexer.py
+++ safe_transaction_service/history/indexers/ethereum_indexer.py
@@ -209,17 +209,16 @@ class EthereumIndexer(ABC):
                 logger.info('%s: block_process_limit halved to %d', self.__class__.__name__,
                             self.block_process_limit)
             if time_diff > 10:
-                new_block_process_limit = self.block_process_limit - 10000
-                if new_block_process_limit > 0:
-                    self.block_process_limit = new_block_process_limit
-                    logger.info('%s: block_process_limit decreased to %d', self.__class__.__name__,
-                                self.block_process_limit)
+                new_block_process_limit = max(self.block_process_limit - 100, 1)
+                self.block_process_limit = new_block_process_limit
+                logger.info('%s: block_process_limit decreased to %d', self.__class__.__name__,
+                            self.block_process_limit)
             elif time_diff < 1:
                 self.block_process_limit *= 2
                 logger.info('%s: block_process_limit duplicated to %d', self.__class__.__name__,
                             self.block_process_limit)
             elif time_diff < 3:
-                self.block_process_limit += 10000
+                self.block_process_limit += 100
                 logger.info('%s: block_process_limit increased to %d', self.__class__.__name__,
                             self.block_process_limit)
 
