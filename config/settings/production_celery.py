dd21e81d814e7acbc94ee550090a885c33a340ae
Uxio Fuentefria <uxio@gnosis.pm>
2020-10-27 11:36:14 +0100

    Don't duplicate logs on celery

--- config/settings/production_celery.py
+++ config/settings/production_celery.py
@@ -4,4 +4,6 @@ from .production import *  # noqa
 # Patch all the code to use Celery logger (if not just logs inside tasks.py are displayed with the
 # task_id and task_name). This way every log will have the context information
 for _, logger in LOGGING['loggers'].items():
-    logger['handlers'] = ['celery_console']
+    key = 'handlers'
+    if key in logger:
+        logger[key] = ['celery_console']
