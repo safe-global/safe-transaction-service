27af0788e0aeb5dd606f33607947e4a5af0dc847
Uxio Fuentefria <uxio@gnosis.pm>
2020-11-02 14:47:24 +0100

    Rename Django `url` -> `re_path`

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
 from django.http import HttpResponse
-from django.urls import path
+from django.urls import path, re_path
 from django.views import defaults as default_views
 from django.views.decorators.cache import cache_control
 
@@ -25,18 +25,18 @@ schema_cache_timeout = 60 * 5  # 5 minutes
 schema_cache_decorator = cache_control(max_age=schema_cache_timeout)
 
 urlpatterns = [
-    url(r'^$',
-        schema_cache_decorator(schema_view.with_ui('swagger', cache_timeout=schema_cache_timeout)),
-        name='schema-swagger-ui'),
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_cache_decorator(schema_view.without_ui(cache_timeout=schema_cache_timeout)),
-        name='schema-json'),
-    url(r'^redoc/$',
-        schema_cache_decorator(schema_view.with_ui('redoc', cache_timeout=schema_cache_timeout)),
-        name='schema-redoc'),
-    url(settings.ADMIN_URL, admin.site.urls),
-    url(r'^api/v1/', include('safe_transaction_service.history.urls', namespace='v1')),
-    url(r'^check/', lambda request: HttpResponse("Ok"), name='check'),
+    re_path(r'^$',
+            schema_cache_decorator(schema_view.with_ui('swagger', cache_timeout=schema_cache_timeout)),
+            name='schema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_cache_decorator(schema_view.without_ui(cache_timeout=schema_cache_timeout)),
+            name='schema-json'),
+    re_path(r'^redoc/$',
+            schema_cache_decorator(schema_view.with_ui('redoc', cache_timeout=schema_cache_timeout)),
+            name='schema-redoc'),
+    re_path(settings.ADMIN_URL, admin.site.urls),
+    re_path(r'^api/v1/', include('safe_transaction_service.history.urls', namespace='v1')),
+    re_path(r'^check/', lambda request: HttpResponse("Ok"), name='check'),
 ]
 
 
@@ -44,22 +44,22 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(
+        re_path(
             r"^400/$",
             default_views.bad_request,
             kwargs={"exception": Exception("Bad Request!")},
         ),
-        url(
+        re_path(
             r"^403/$",
             default_views.permission_denied,
             kwargs={"exception": Exception("Permission Denied")},
         ),
-        url(
+        re_path(
             r"^404/$",
             default_views.page_not_found,
             kwargs={"exception": Exception("Page not Found")},
         ),
-        url(r"^500/$", default_views.server_error),
+        re_path(r"^500/$", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
