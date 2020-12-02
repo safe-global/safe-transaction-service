a65a74b9022a2904fc25d17a2df76e98d5061381
Uxio Fuentefria <uxio@gnosis.pm>
2020-12-02 18:22:33 +0100

    WIP
    - Add new model FirebaseDeviceOwner
    - Update serializers
    - Add signature logic

--- safe_transaction_service/notifications/serializers.py
+++ safe_transaction_service/notifications/serializers.py
@@ -1,27 +1,25 @@
 import time
-import uuid
 from typing import Any, Dict, Sequence
+from uuid import uuid4
 
 from django.db import IntegrityError
 
-from hexbytes import HexBytes
 from packaging import version as semantic_version
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from web3 import Web3
 
 from gnosis.eth.django.serializers import (EthereumAddressField,
                                            HexadecimalField)
-from gnosis.safe.safe_signature import SafeSignature
+from gnosis.safe.safe_signature import SafeSignature, SafeSignatureType
 
 from safe_transaction_service.history.models import SafeContract
 
-from .models import DeviceTypeEnum, FirebaseDevice
-from .utils import get_safe_owners
+from .models import DeviceTypeEnum, FirebaseDevice, FirebaseDeviceOwner
+from .utils import calculate_device_registration_hash, get_safe_owners
 
 
 class FirebaseDeviceSerializer(serializers.Serializer):
-    uuid = serializers.UUIDField(default=uuid.uuid4)  # TODO Make it required
+    uuid = serializers.UUIDField(default=uuid4)  # TODO Make it required
     safes = serializers.ListField(allow_empty=False, child=EthereumAddressField())
     cloud_messaging_token = serializers.CharField(min_length=100, max_length=200)
     build_number = serializers.IntegerField(min_value=0)  # e.g. 1644
@@ -31,15 +29,8 @@ class FirebaseDeviceSerializer(serializers.Serializer):
     timestamp = serializers.IntegerField(required=False)  # TODO Make it required
     signatures = serializers.ListField(
         required=False,
-        child=HexadecimalField(required=False, min_length=130)  # Signatures must be at least 65 bytes
+        child=HexadecimalField(required=False, min_length=130, max_length=130)  # Signatures must be 65 bytes
     )
-
-    def _calculate_hash(self, timestamp: int, identifier: uuid.UUID, cloud_messaging_token: str,
-                        safes: Sequence[str], prefix: str = 'gnosis-safe') -> HexBytes:
-
-        safes_to_str = ''.join(sorted(safes))
-        str_to_sign = f'{prefix}{timestamp}{identifier}{cloud_messaging_token}{safes_to_str}'
-        return Web3.keccak(text=str_to_sign)
 
     def validate_safes(self, safes: Sequence[str]):
         if SafeContract.objects.filter(address__in=safes).count() != len(safes):
@@ -71,23 +62,35 @@ class FirebaseDeviceSerializer(serializers.Serializer):
 
     def validate(self, data: Dict[str, Any]):
         data = super().validate(data)
-        data['owners'] = {}
+        data['owners'] = []
         signatures = data.get('signatures') or []
         if signatures:
             current_owners = {owner for safe in data['safes'] for owner in get_safe_owners(safe)}
             for signature in signatures:
-                hash_to_sign = self._calculate_hash(data['timestamp'], data['uuid'], data['cloud_messaging_token'],
-                                                    data['safes'])
+                hash_to_sign = calculate_device_registration_hash(data['timestamp'],
+                                                                  data['uuid'],
+                                                                  data['cloud_messaging_token'],
+                                                                  data['safes'])
                 parsed_signatures = SafeSignature.parse_signature(signature, hash_to_sign)
+                if not parsed_signatures:
+                    raise ValidationError('Signature cannot be parsed')
                 for safe_signature in parsed_signatures:
-                    if (owner := safe_signature.owner) in current_owners:
-                        data['owners'].add(owner)
+                    if safe_signature.signature_type != SafeSignatureType.EOA or not safe_signature.is_valid():
+                        raise ValidationError('An externally owned account signature was expected')
+                    owner = safe_signature.owner
+                    if owner in data['owners']:
+                        raise ValidationError(f'Signature for owner={owner} is duplicated')
+                    elif owner not in current_owners:
+                        raise ValidationError(f'Owner={owner} is not an owner of any of the safes={data["safes"]}')
+                    else:
+                        data['owners'].append(owner)
         return data
 
     def save(self, **kwargs):
         try:
+            uuid = self.validated_data['uuid']
             firebase_device, _ = FirebaseDevice.objects.update_or_create(
-                uuid=self.validated_data['uuid'],
+                uuid=uuid,
                 defaults={
                     'cloud_messaging_token': self.validated_data['cloud_messaging_token'],
                     'build_number': self.validated_data['build_number'],
@@ -96,8 +99,19 @@ class FirebaseDeviceSerializer(serializers.Serializer):
                     'version': self.validated_data['version'],
                 }
             )
-        except IntegrityError:
+        except IntegrityError as e:
             raise serializers.ValidationError('Cloud messaging token is linked to another device')
+
+        # Remove every owner registered for the device and add the provided ones
+        firebase_device.owners.all().delete()
+        for owner in self.validated_data['owners']:
+            FirebaseDeviceOwner.objects.create(
+                firebase_device=firebase_device,
+                owner=owner
+            )
+
+        # Remove every Safe registered for the device and add the provided ones
+        firebase_device.safes.clear()
         safe_contracts = SafeContract.objects.filter(address__in=self.validated_data['safes'])
         firebase_device.safes.add(*safe_contracts)
         return firebase_device
--- safe_transaction_service/notifications/utils.py
+++ safe_transaction_service/notifications/utils.py
@@ -1,5 +1,8 @@
 from typing import Sequence
+from uuid import UUID
 
+from hexbytes import HexBytes
+from web3 import Web3
 from web3.exceptions import BadFunctionCallOutput
 
 from gnosis.eth import EthereumClientProvider
@@ -12,4 +15,14 @@ def get_safe_owners(safe_address: str) -> Sequence[str]:
     try:
         return safe.retrieve_owners(block_identifier='pending')
     except BadFunctionCallOutput:  # Error using pending block identifier
-        return safe.retrieve_owners(block_identifier='latest')
+        try:
+            return safe.retrieve_owners(block_identifier='latest')
+        except BadFunctionCallOutput:
+            return []
+
+
+def calculate_device_registration_hash(timestamp: int, identifier: UUID, cloud_messaging_token: str,
+                                       safes: Sequence[str], prefix: str = 'gnosis-safe') -> HexBytes:
+    safes_to_str = ''.join(sorted(safes))
+    str_to_sign = f'{prefix}{timestamp}{identifier}{cloud_messaging_token}{safes_to_str}'
+    return Web3.keccak(text=str_to_sign)
