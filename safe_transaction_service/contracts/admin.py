1b68a7e26bdb2fc23c383e79e3ecc4995b247a8c
Uxio Fuentefria <uxio@gnosis.pm>
2020-12-10 14:19:20 +0100

    Improve admin

--- safe_transaction_service/contracts/admin.py
+++ safe_transaction_service/contracts/admin.py
@@ -8,9 +8,29 @@ class ContractAbiAdmin(admin.ModelAdmin):
     list_display = ('description', 'abi')
 
 
+class HasAbiFilter(admin.SimpleListFilter):
+    title = 'Has ABI'
+    parameter_name = 'has_abi'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('YES', 'Yes'),
+            ('NO', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'NO':
+            return queryset.filter(contract_abi=None)
+        elif self.value() == 'YES':
+            return queryset.exclude(contract_abi=None)
+        else:
+            return queryset
+
+
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
     list_display = ('address', 'name', 'has_abi')
+    list_filter = (HasAbiFilter,)
     ordering = ['address']
     raw_id_fields = ('contract_abi',)
     search_fields = ['address', 'name', 'contract_abi__abi', 'contract_abi__description']
