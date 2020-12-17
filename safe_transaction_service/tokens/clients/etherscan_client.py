bd64b17d487468362aac392a2877dc758c0487ac
Uxio Fuentefria <uxio@gnosis.pm>
2020-12-17 14:10:49 +0100

    Get contract_info using etherscan scraper

--- safe_transaction_service/tokens/clients/etherscan_client.py
+++ safe_transaction_service/tokens/clients/etherscan_client.py
@@ -1,7 +1,19 @@
 from dataclasses import dataclass
-from typing import ClassVar, List, Literal, Sequence
+from functools import cached_property
+from typing import ClassVar, List, Literal, Optional, Sequence
 
 from eth_utils import to_checksum_address
+from hexbytes import HexBytes
+
+
+@dataclass
+class EtherscanContract:
+    name: str
+    code: str
+    abi: str
+    contract_creation_code: Optional[HexBytes]
+    compiler_version: str
+    optimization_enabled: str
 
 
 @dataclass
@@ -12,7 +24,31 @@ class EtherscanToken:
 
 
 class EtherscanClient:
+    address_url: ClassVar[str] = 'https://etherscan.io/address'
     tokens_url: ClassVar[str] = 'https://etherscan.io/tokens'
+
+    @cached_property
+    def scraper(self):
+        import cfscrape
+        return cfscrape.create_scraper()  # Bypass cloudfare
+
+    def _parse_contracts_page(self, content: bytes) -> Optional[EtherscanContract]:
+        from lxml import html
+        tree = html.fromstring(content)
+
+        if not tree.xpath('//*[@id="editor"]'):
+            return None
+
+        name = tree.xpath('//*[@id="ContentPlaceHolder1_contractCodeDiv"]/div[2]/div[1]/div[1]/div[2]/span')[0].text
+        code = tree.xpath('//*[@id="editor"]')[0].text_content()
+        abi = tree.xpath('//*[@id="js-copytextarea2"]')[0].text
+        contract_creation_code_tree = tree.xpath('//*[@id="verifiedbytecode2"]')
+        contract_creation_code = HexBytes(contract_creation_code_tree[0].text) if contract_creation_code_tree else None
+        compiler_version = tree.xpath('//*[@id="ContentPlaceHolder1_contractCodeDiv"]/div[2]/div[1]/div[2]/div[2]/span')[0].text
+        optimization_enabled = tree.xpath('//*[@id="ContentPlaceHolder1_contractCodeDiv"]/div[2]/div[2]/div[1]/div[2]/span')[0].text_content()
+
+        return EtherscanContract(name, code, abi, contract_creation_code, compiler_version,
+                                 optimization_enabled)
 
     def _parse_tokens_page(self, content: bytes) -> Sequence[EtherscanToken]:
         from lxml import html
@@ -27,6 +63,10 @@ class EtherscanClient:
             address = to_checksum_address(etherscan_url.replace('/token/', ''))
             tokens.append(EtherscanToken(name, address, etherscan_url))
         return tokens
+
+    def get_contract_info(self, address: str) -> Optional[EtherscanContract]:
+        response = self.scraper.get(f'{self.address_url}/{address}')
+        return self._parse_contracts_page(response.content)
 
     def get_tokens_page(self, page: int = 1,
                         elements: Literal[10, 25, 50, 100] = 100) -> Sequence[EtherscanToken]:
