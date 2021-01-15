b87a7f5668990ed54dca7241afe1455802341635
Uxio Fuentefria <uxio@gnosis.pm>
2021-01-15 14:18:59 +0100

    Add missing script

---
+++ scripts/deploy_docker_github.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$1" = "develop" -o "$1" = "master" ]; then
+    # If image does not exist, don't use cache
+    docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+else
+    docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+fi
+docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
+docker push gnosispm/$DOCKERHUB_PROJECT:$1
