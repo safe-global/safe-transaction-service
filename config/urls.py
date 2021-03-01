f4fd10879a350ae8fef5b0b6e217426390c8fe82
Uxio Fuentefria <uxio@gnosis.pm>
2021-03-01 10:02:14 +0100

    Remove cache from drf yasg
    - Seems to have some issues

--- config/urls.py
+++ config/urls.py
@@ -4,35 +4,35 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, re_path
 from django.views import defaults as default_views
-from django.views.decorators.cache import cache_control
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title='Gnosis Safe Transaction Service API',
         default_version='v1',
-        description='API to store safe multisig transactions',
+        description='API to keep track of transactions sent via Gnosis Safe smart contracts',
         contact=openapi.Contact(email='safe@gnosis.io'),
         license=openapi.License(name='MIT License'),
     ),
     validators=['flex', 'ssv'],
     public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 schema_cache_timeout = 60 * 5  # 5 minutes
-schema_cache_decorator = cache_control(max_age=schema_cache_timeout)
 
 urlpatterns = [
     re_path(r'^$',
-            schema_cache_decorator(schema_view.with_ui('swagger', cache_timeout=schema_cache_timeout)),
+            schema_view.with_ui('swagger', cache_timeout=schema_cache_timeout),
             name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_cache_decorator(schema_view.without_ui(cache_timeout=schema_cache_timeout)),
+            schema_view.without_ui(cache_timeout=schema_cache_timeout),
             name='schema-json'),
     re_path(r'^redoc/$',
-            schema_cache_decorator(schema_view.with_ui('redoc', cache_timeout=schema_cache_timeout)),
+            schema_view.with_ui('redoc', cache_timeout=schema_cache_timeout),
             name='schema-redoc'),
     re_path(settings.ADMIN_URL, admin.site.urls),
     re_path(r'^api/v1/', include('safe_transaction_service.history.urls', namespace='v1')),
