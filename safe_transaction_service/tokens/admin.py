af428687928cbadbcb26cf6c4fda2d1019f3105c
Uxio Fuentefria <uxio@gnosis.pm>
2021-03-31 12:52:48 +0200

    Show decimals filter last in tokens admin

--- safe_transaction_service/tokens/admin.py
+++ safe_transaction_service/tokens/admin.py
@@ -6,6 +6,6 @@ from .models import Token
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
     list_display = ('address', 'trusted', 'spam', 'events_bugged', 'name', 'symbol', 'decimals')
-    list_filter = ('decimals', 'trusted', 'spam', 'events_bugged')
+    list_filter = ('trusted', 'spam', 'events_bugged', 'decimals')
     ordering = ('name',)
     search_fields = ['symbol', 'address', 'name']
