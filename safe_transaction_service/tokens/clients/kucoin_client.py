771257f8f861fa98d3956a8f74c0c758444afdb2
Uxio Fuentefria <uxio@gnosis.pm>
2021-04-05 16:36:29 +0200

    Fix typo on Kucoin price client

--- safe_transaction_service/tokens/clients/kucoin_client.py
+++ safe_transaction_service/tokens/clients/kucoin_client.py
@@ -8,13 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 class KucoinClient:
+    PRICE_URL = 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=EWT-USDT'
+
     def __init__(self):
         self.http_session = requests.session()
 
     def get_ewt_usd_price(self) -> float:
-        url = 'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=EWT-USDT'
-        response = self.http_session.get(url)
         try:
+            response = self.http_session.get(self.PRICE_URL)
             result = response.json()
             return float(result['data']['price'])
         except (ValueError, IOError) as e:
