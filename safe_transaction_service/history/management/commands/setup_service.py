479e74dfba93c896b82a02e0d2820e356fb43a5e
Uxio Fuentefria <uxio@gnosis.pm>
2021-05-11 17:59:11 +0200

    Add L2 Safes to setup service

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -1,5 +1,7 @@
-from typing import Dict, List, NamedTuple, Sequence, Tuple
+from dataclasses import dataclass
+from typing import Dict, List, Sequence, Tuple
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from django_celery_beat.models import IntervalSchedule, PeriodicTask
@@ -7,32 +9,35 @@ from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from gnosis.eth import EthereumClientProvider
 from gnosis.eth.ethereum_client import EthereumNetwork
 
-from ...models import ProxyFactory, SafeMasterCopy
+from ...models import ProxyFactory, SafeL2MasterCopy, SafeMasterCopy
 
 
-class CeleryTaskConfiguration(NamedTuple):
+@dataclass
+class CeleryTaskConfiguration:
     name: str
     description: str
     interval: int
     period: str
+    enabled: bool = True
 
     def create_task(self) -> Tuple[PeriodicTask, bool]:
-        interval, _ = IntervalSchedule.objects.get_or_create(every=self.interval, period=self.period)
-        periodic_task, created = PeriodicTask.objects.get_or_create(task=self.name,
-                                                                    defaults={
-                                                                        'name': self.description,
-                                                                        'interval': interval
-                                                                    })
-        if periodic_task.interval != interval:
-            periodic_task.interval = interval
-            periodic_task.save(update_fields=['interval'])
-
+        interval_schedule, _ = IntervalSchedule.objects.get_or_create(every=self.interval, period=self.period)
+        periodic_task, created = PeriodicTask.objects.update_or_create(task=self.name,
+                                                                       defaults={
+                                                                           'name': self.description,
+                                                                           'interval': interval_schedule,
+                                                                           'enabled': self.enabled,
+                                                                       })
         return periodic_task, created
 
 
 TASKS = [
     CeleryTaskConfiguration('safe_transaction_service.history.tasks.index_internal_txs_task',
-                            'Index Internal Txs', 13, IntervalSchedule.SECONDS),
+                            'Index Internal Txs', 13, IntervalSchedule.SECONDS,
+                            bool(settings.ETHEREUM_TRACING_NODE_URL)),  # Enabled if tracing node configured
+    CeleryTaskConfiguration('safe_transaction_service.history.tasks.index_safe_events_task',
+                            'Index Safe events (L2)', 13, IntervalSchedule.SECONDS,
+                            not settings.ETHEREUM_TRACING_NODE_URL),    # Enabled if tracing node not configured
     # CeleryTaskConfiguration('safe_transaction_service.history.tasks.index_new_proxies_task',
     #                        'Index new Proxies', 15, IntervalSchedule.SECONDS),
     CeleryTaskConfiguration('safe_transaction_service.history.tasks.index_erc20_events_task',
@@ -47,6 +52,7 @@ TASKS = [
 
 MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.MAINNET: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10329734, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 9084503, '1.1.1'),
         ('0xaE32496491b53841efb51829d6f886387708F99B', 8915728, '1.1.0'),
@@ -55,6 +61,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0xAC6072986E985aaBE7804695EC2d8970Cf7541A2', 6569433, '0.0.2'),
     ],
     EthereumNetwork.RINKEBY: [
+        ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 8527381, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6723632, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 5590754, '1.1.1'),
         ('0xaE32496491b53841efb51829d6f886387708F99B', 5423491, '1.1.0'),
@@ -63,6 +70,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0x2727D69C0BD14B1dDd28371B8D97e808aDc1C2f7', 3055781, '0.0.2'),
     ],
     EthereumNetwork.GOERLI: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 2930373, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 1798663, '1.1.1'),
         ('0xaE32496491b53841efb51829d6f886387708F99B', 1631488, '1.1.0'),
@@ -70,6 +78,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 34096, '0.1.0'),
     ],
     EthereumNetwork.KOVAN: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 19242615, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 15366145, '1.1.1'),
         ('0xaE32496491b53841efb51829d6f886387708F99B', 14740724, '1.1.0'),
@@ -77,19 +86,45 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0x8942595A2dC5181Df0465AF0D7be08c8f23C93af', 9465686, '0.1.0'),
     ],
     EthereumNetwork.XDAI: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 10612049, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 10045292, '1.1.1'),
         ('0x2CB0ebc503dE87CFD8f0eCEED8197bF7850184ae', 12529466, '1.1.1-Circles'),
     ],
     EthereumNetwork.ENERGY_WEB_CHAIN: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6398655, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6399212, '1.1.1'),
     ],
     EthereumNetwork.VOLTA: [
+        # ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 0, '1.3.0'),
         ('0x6851D6fDFAfD08c0295C392436245E5bc78B0185', 6876086, '1.2.0'),
         ('0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F', 6876642, '1.1.1'),
     ]
+}
 
+L2_MASTER_COPIES = {
+    EthereumNetwork.MAINNET: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ],
+    EthereumNetwork.RINKEBY: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 8527380, '1.3.0')
+    ],
+    EthereumNetwork.GOERLI: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ],
+    EthereumNetwork.KOVAN: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ],
+    EthereumNetwork.XDAI: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ],
+    EthereumNetwork.ENERGY_WEB_CHAIN: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ],
+    EthereumNetwork.VOLTA: [
+        ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 0, '1.3.0')
+    ]
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -142,6 +177,9 @@ class Command(BaseCommand):
         if ethereum_network in MASTER_COPIES:
             self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} safe addresses'))
             self._setup_safe_master_copies(MASTER_COPIES[ethereum_network])
+        if ethereum_network in MASTER_COPIES:
+            self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} l2 safe addresses'))
+            self._setup_safe_l2_master_copies(L2_MASTER_COPIES[ethereum_network])
         if ethereum_network in PROXY_FACTORIES:
             self.stdout.write(self.style.SUCCESS(f'Setting up {ethereum_network.name} proxy factory addresses'))
             self._setup_safe_proxy_factories(PROXY_FACTORIES[ethereum_network])
@@ -152,6 +190,21 @@ class Command(BaseCommand):
     def _setup_safe_master_copies(self, safe_master_copies: Sequence[Tuple[str, int, str]]):
         for address, initial_block_number, version in safe_master_copies:
             safe_master_copy, _ = SafeMasterCopy.objects.get_or_create(
+                address=address,
+                defaults={
+                    'initial_block_number': initial_block_number,
+                    'tx_block_number': initial_block_number,
+                    'version': version,
+                }
+            )
+            if safe_master_copy.version != version:
+                safe_master_copy.version = version
+                safe_master_copy.save(update_fields=['version'])
+
+    def _setup_safe_l2_master_copies(self, safe_master_copies: Sequence[Tuple[str, int, str]]):
+        # TODO Refactor  _setup_safe_master_copies and _setup_safe_l2_master_copies into one method
+        for address, initial_block_number, version in safe_master_copies:
+            safe_master_copy, _ = SafeL2MasterCopy.objects.get_or_create(
                 address=address,
                 defaults={
                     'initial_block_number': initial_block_number,
