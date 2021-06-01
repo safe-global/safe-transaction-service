9846f1dde3c1a301b796b5cd4c9dbecb2dee0f70
Uxio Fuentefria <uxio@gnosis.pm>
2021-06-01 16:57:47 +0200

    Improve export_multisig_tx_data command

--- safe_transaction_service/history/management/commands/export_multisig_tx_data.py
+++ safe_transaction_service/history/management/commands/export_multisig_tx_data.py
@@ -27,7 +27,12 @@ class Command(BaseCommand):
         if count:
             with open(file_name, 'w') as f:
                 decoder = get_db_tx_decoder()
+                f.write('|'.join(['Execution date', 'tx-hash', 'safe-address', 'gas_price', 'gas_limit', 'gas_used',
+                                  'to', 'failed', 'origin', 'decodedData']) + '\n')
                 for m in queryset.select_related('ethereum_tx__block'):
-                    f.write('|'.join([str(m.execution_date), m.ethereum_tx_id, m.safe, m.to, str(m.failed), m.origin,
+                    ethereum_tx = m.ethereum_tx
+                    f.write('|'.join([str(m.execution_date), m.ethereum_tx_id, m.safe, str(ethereum_tx.gas_price),
+                                      str(m.ethereum_tx.gas), str(m.ethereum_tx.gas_used),
+                                      m.to, str(m.failed), m.origin,
                                       json.dumps(decoder.get_data_decoded(m.data.tobytes())) if m.data else '']) + '\n')
             self.stdout.write(self.style.SUCCESS(f'Multisig tx data was exported to {file_name}'))
