512464a2d230addb288ba01bf7d3334f8f480b37
Uxio Fuentefria <uxio@gnosis.pm>
2021-06-07 20:41:09 +0200

    Improve logging of sync_contract_abis command

--- safe_transaction_service/contracts/management/commands/sync_contract_abis.py
+++ safe_transaction_service/contracts/management/commands/sync_contract_abis.py
@@ -1,8 +1,14 @@
+import time
+
 from django.core.management.base import BaseCommand
 
 from gnosis.eth import EthereumClientProvider
 
-from ...models import Contract
+from safe_transaction_service.tokens.clients import EtherscanClient
+from safe_transaction_service.tokens.clients.etherscan_client import \
+    EtherscanClientException
+
+from ...models import Contract, ContractAbi
 
 
 class Command(BaseCommand):
@@ -11,9 +17,13 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--all', help="Sync contract names/ABIS for contracts already synced", action='store_true',
                             default=False)
+        parser.add_argument('--scraper', help="Scrape etherscan instead of using API (to have names)",
+                            action='store_true', default=False)
 
     def handle(self, *args, **options):
         every_contract = options['all']
+        scraper = options['scraper']
+        etherscan_client = EtherscanClient() if scraper else None
 
         ethereum_client = EthereumClientProvider()
         network = ethereum_client.get_network()
@@ -23,5 +33,25 @@ class Command(BaseCommand):
             contract_queryset = contract_queryset.filter(contract_abi=None)
 
         for contract in contract_queryset:
-            if contract.sync_abi_from_api(network=network):
+            updated = False
+            if not etherscan_client:
+                updated = contract.sync_abi_from_api(network=network)
+            else:  # Use etherscan scraper
+                try:
+                    contract_info = etherscan_client.get_contract_info(contract.address)
+                    if contract_info:
+                        contract_abi, _ = ContractAbi.objects.update_or_create(abi=contract_info.abi,
+                                                                               defaults={
+                                                                                   'description': contract_info.name
+                                                                               })
+                        contract, _ = Contract.objects.update_or_create(address=contract_info.abi,
+                                                                        defaults={
+                                                                            'name': contract_info.name,
+                                                                            'abi': contract_abi
+                                                                        })
+                        updated = True
+                except EtherscanClientException:
+                    time.sleep(5)
+
+            if updated:
                 self.stdout.write(self.style.SUCCESS(f'Synced contract {contract.address} - {contract.name}'))
