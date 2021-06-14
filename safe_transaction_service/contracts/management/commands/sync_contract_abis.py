2b7d3f55fa23e18f3f514b2b78aa65c26565d3d8
Uxio Fuentefria <uxio@gnosis.pm>
2021-06-14 12:32:59 +0200

    Remove etherscan scraper

--- safe_transaction_service/contracts/management/commands/sync_contract_abis.py
+++ safe_transaction_service/contracts/management/commands/sync_contract_abis.py
@@ -1,14 +1,8 @@
-import time
-
 from django.core.management.base import BaseCommand
 
 from gnosis.eth import EthereumClientProvider
 
-from safe_transaction_service.tokens.clients import EtherscanClient
-from safe_transaction_service.tokens.clients.etherscan_client import \
-    EtherscanClientException
-
-from ...models import Contract, ContractAbi
+from ...models import Contract
 
 
 class Command(BaseCommand):
@@ -17,13 +11,9 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--all', help="Sync contract names/ABIS for contracts already synced", action='store_true',
                             default=False)
-        parser.add_argument('--scraper', help="Scrape etherscan instead of using API (to have names)",
-                            action='store_true', default=False)
 
     def handle(self, *args, **options):
         every_contract = options['all']
-        scraper = options['scraper']
-        etherscan_client = EtherscanClient() if scraper else None
 
         ethereum_client = EthereumClientProvider()
         network = ethereum_client.get_network()
@@ -33,22 +23,5 @@ class Command(BaseCommand):
             contract_queryset = contract_queryset.filter(contract_abi=None)
 
         for contract in contract_queryset:
-            if not etherscan_client:
-                if contract.sync_abi_from_api(network=network):
-                    self.stdout.write(self.style.SUCCESS(f'Synced contract {contract.address} - {contract.name}'))
-            else:  # Use etherscan scraper
-                try:
-                    contract_info = etherscan_client.get_contract_info(contract.address)
-                    if contract_info:
-                        contract_abi, _ = ContractAbi.objects.update_or_create(abi=contract_info.abi,
-                                                                               defaults={
-                                                                                   'description': contract_info.name
-                                                                               })
-                        contract, _ = Contract.objects.update_or_create(address=contract_info.abi,
-                                                                        defaults={
-                                                                            'name': contract_info.name,
-                                                                            'abi': contract_abi
-                                                                        })
-                        self.stdout.write(self.style.SUCCESS(f'Synced contract {contract.address} - {contract.name}'))
-                except EtherscanClientException:
-                    time.sleep(5)
+            if contract.sync_abi_from_api(network=network):
+                self.stdout.write(self.style.SUCCESS(f'Synced contract {contract.address} - {contract.name}'))
--- safe_transaction_service/tokens/clients/__init__.py
+++ safe_transaction_service/tokens/clients/__init__.py
@@ -2,7 +2,6 @@
 from .binance_client import BinanceClient
 from .coingecko_client import CoingeckoClient
 from .coinmarketcap_client import CoinMarketCapClient, CoinMarketCapToken
-from .etherscan_client import EtherscanClient, EtherscanToken
 from .exceptions import CannotGetPrice
 from .kleros_client import KlerosClient, KlerosToken
 from .kraken_client import KrakenClient
