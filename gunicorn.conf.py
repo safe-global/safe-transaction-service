08c1519cc603f9b357f00e03d8840489f86b9006
Uxio Fuentefria <uxio@gnosis.pm>
2021-06-21 17:21:45 +0200

    Increase gunicorn worker timeout

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,12 @@
 access_logfile = '-'
 error_logfile = '-'
-graceful_timeout = 60
+graceful_timeout = 120
 log_file = '-'
 log_level = 'info'
 logger_class = 'safe_transaction_service.utils.loggers.CustomGunicornLogger'
 # max_requests = 2000  # Restart a worker after it has processed a given number of requests (for memory leaks)
 preload_app = False  # Load application code before the worker processes are forked (problems with gevent patching)
-timeout = 60
+timeout = 90
 worker_class = 'gevent'
 worker_connections = 2000
 
