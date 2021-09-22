26604ed05ad3f5a416714855cd4026ef48fb16a0
Uxío <Uxio0@users.noreply.github.com>
2021-09-22 16:59:59 +0200

    Support CELO Safe deployments (#385)
    - https://github.com/gnosis/safe-deployments/pull/18

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -139,6 +139,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0x3E5c63644E683549055b9Be8653de26E0B4CD36E', 8485899, '1.3.0+L2'),
         ('0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552', 8485903, '1.3.0'),
     ],
+    EthereumNetwork.CELO: [
+        ('0xfb1bffC9d739B8D520DaF37dF666da4C687191EA', 8944350, '1.3.0+L2'),
+        ('0x69f4D1788e39c87893C980c06EdF4b7f686e2938', 8944351, '1.3.0'),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -192,6 +196,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.BINANCE: [
         ('0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2', 8485873),  # v1.3.0
+    ],
+    EthereumNetwork.CELO: [
+        ('0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC', 8944342),  # v1.3.0
     ],
 }
 
