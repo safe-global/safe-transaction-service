61ee03ee2f712941c2b319d1cf2240c414a2177c
nambrot <nambrot@googlemail.com>
2021-09-24 16:35:43 -0400

    set Allowed hosts

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,11 +10,11 @@ DEBUG = env.bool('DEBUG', default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='aHdCBMHXuxIxEhfRGFRp7Cp3N9CqEZEEAvwZVlBCazKExkEnzvVs4bYWC8Qqh9lg')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
     "localhost",
     "0.0.0.0",
     "127.0.0.1",
-]
+])
 
 # CACHES
 # ------------------------------------------------------------------------------
