62683e76faaa1610a8e89739de2cf22f83ebf317
Uxio Fuentefria <uxio@gnosis.pm>
2021-09-30 13:10:23 +0200

    Support AVALANCHE Safe contracts

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -143,6 +143,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ('0xfb1bffC9d739B8D520DaF37dF666da4C687191EA', 8944350, '1.3.0+L2'),
         ('0x69f4D1788e39c87893C980c06EdF4b7f686e2938', 8944351, '1.3.0'),
     ],
+    EthereumNetwork.AVALANCHE: [
+        ('0xfb1bffC9d739B8D520DaF37dF666da4C687191EA', 4_949_507, '1.3.0+L2'),
+        ('0x69f4D1788e39c87893C980c06EdF4b7f686e2938', 4_949_512, '1.3.0'),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -199,6 +203,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.CELO: [
         ('0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC', 8944342),  # v1.3.0
+    ],
+    EthereumNetwork.AVALANCHE: [
+        ('0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC', 4_949_487),  # v1.3.0
     ],
 }
 
