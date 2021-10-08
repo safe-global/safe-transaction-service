c91c43d10ff1952b7292e4148692dde9e25e8d20
Uxio Fuentefria <uxio@gnosis.pm>
2021-10-08 12:29:05 +0200

    Add minor improvements for testing

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,13 @@
 
 set -euo pipefail
 
+export DJANGO_SETTINGS_MODULE=config.settings.test
+export DJANGO_DOT_ENV_FILE=.env.test
 docker compose -f docker-compose.yml -f docker-compose.dev.yml build --force-rm db redis ganache
 docker compose -f docker-compose.yml -f docker-compose.dev.yml up --no-start db redis ganache
-docker compose -f docker-compose.yml -f docker-compose.dev.yml restart db redis ganache
+docker compose -f docker-compose.yml -f docker-compose.dev.yml start db redis ganache
+
 sleep 10
-DJANGO_SETTINGS_MODULE=config.settings.test DJANGO_DOT_ENV_FILE=.env.test python manage.py check
-DJANGO_SETTINGS_MODULE=config.settings.test DJANGO_DOT_ENV_FILE=.env.test pytest -rxXs
+
+python manage.py check
+pytest -rxXs
