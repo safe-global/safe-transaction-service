d19cbcd9dc53ce9ec7b6eb1ca78e6af49b7d9ecf
Uxio Fuentefria <uxio@gnosis.pm>
2021-10-22 12:07:29 +0200

    Add task to index ERC events for Safes out of sync

--- safe_transaction_service/history/management/commands/index_erc20.py
+++ safe_transaction_service/history/management/commands/index_erc20.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from ...indexers import Erc20EventsIndexerProvider, FindRelevantElementsException
+from ...tasks import index_erc20_events_out_of_sync_task
 
 
 class Command(BaseCommand):
@@ -28,47 +28,37 @@ class Command(BaseCommand):
             help="Max number of blocks to query each time",
             default=None,
         )
+        parser.add_argument(
+            "--sync",
+            help="Don't trigger an async task",
+            action="store_true",
+            default=False,
+        )
 
     def handle(self, *args, **options):
-        erc20_events_indexer = Erc20EventsIndexerProvider()
+        addresses = options["addresses"]
+        number_of_addresses = options["number_of_addresses"]
+        sync = options["sync"]
+
         if block_process_limit := options["block_process_limit"]:
             self.stdout.write(
                 self.style.SUCCESS(
                     f"Setting block-process-limit to {block_process_limit}"
                 )
             )
-            erc20_events_indexer.block_process_limit = block_process_limit
         if block_process_limit_max := options["block_process_limit_max"]:
-            erc20_events_indexer.block_process_limit_max = block_process_limit_max
             self.stdout.write(
                 self.style.SUCCESS(
                     f"Setting block-process-limit-max to {block_process_limit_max}"
                 )
             )
-
-        current_block_number = erc20_events_indexer.ethereum_client.current_block_number
-        addresses = options["addresses"] or [
-            x.address
-            for x in erc20_events_indexer.get_not_updated_addresses(
-                current_block_number
-            )[: options["number_of_addresses"]]
-        ]
-
-        if not addresses:
-            self.stdout.write(self.style.WARNING("No addresses to process"))
+        arguments = {
+            "block_process_limit": block_process_limit,
+            "block_process_limit_max": block_process_limit_max,
+            "addresses": addresses,
+            "number_of_addresses": number_of_addresses,
+        }
+        if sync:
+            index_erc20_events_out_of_sync_task(**arguments)
         else:
-            self.stdout.write(
-                self.style.SUCCESS(f"Start indexing ERC20 addresses {addresses}")
-            )
-            updated = False
-            while not updated:
-                try:
-                    _, updated = erc20_events_indexer.process_addresses(
-                        addresses, current_block_number
-                    )
-                except FindRelevantElementsException:
-                    pass
-
-            self.stdout.write(
-                self.style.SUCCESS(f"End indexing ERC20 addresses {addresses}")
-            )
+            index_erc20_events_out_of_sync_task.delay(**arguments)
