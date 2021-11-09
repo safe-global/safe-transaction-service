b02c6822fee245ceb975ca6a31ccebe133be3860
Uxio Fuentefria <uxio@gnosis.pm>
2021-11-09 13:07:54 +0100

    Test index_contracts_with_metadata command

--- safe_transaction_service/notifications/tests/test_firebase_client.py
+++ safe_transaction_service/notifications/tests/test_firebase_client.py
@@ -20,41 +20,46 @@ class TestFirebaseClient(TestCase):
         FirebaseClient, "_authenticate", autospec=True, return_value=None
     )
     def test_firebase_client(self, initialize_app_mock):
-        with self.settings(NOTIFICATIONS_FIREBASE_AUTH_CREDENTIALS="ab"):
-            with FirebaseClientPool() as firebase_client:
-                firebase_client.app = None
-                self.assertNotIsInstance(firebase_client, MockedClient)
-                self.assertIsInstance(
-                    firebase_client._build_android_config(), messaging.AndroidConfig
-                )
-                self.assertIsInstance(
-                    firebase_client._build_apns_config(), messaging.APNSConfig
-                )
-                with mock.patch(
-                    "firebase_admin.messaging.send", return_value=True
-                ) as send_mock:
-                    self.assertTrue(firebase_client.verify_token("xy"))
-                    send_mock.side_effect = UnregisteredError("Token not registered")
-                    self.assertFalse(firebase_client.verify_token("xy"))
-
-                responses = [
-                    SendResponse({"name": "id-1"}, None),
-                    SendResponse(
-                        {"name": "id-2"}, UnregisteredError("Token not registered")
-                    ),
-                ]
-                batch_response = BatchResponse(responses)
-                with mock.patch(
-                    "firebase_admin.messaging.send_multicast",
-                    return_value=batch_response,
-                ):
-                    (
-                        success_count,
-                        failure_count,
-                        invalid_tokens,
-                    ) = firebase_client.send_message(
-                        ["token-1", "token-2"], {"tx-hash": "0x-random-hash"}
+        try:
+            with self.settings(NOTIFICATIONS_FIREBASE_AUTH_CREDENTIALS="ab"):
+                with FirebaseClientPool() as firebase_client:
+                    firebase_client.app = None
+                    self.assertNotIsInstance(firebase_client, MockedClient)
+                    self.assertIsInstance(
+                        firebase_client._build_android_config(), messaging.AndroidConfig
                     )
-                    self.assertEqual(success_count, 1)
-                    self.assertEqual(failure_count, 1)
-                    self.assertEqual(invalid_tokens, ["token-2"])
+                    self.assertIsInstance(
+                        firebase_client._build_apns_config(), messaging.APNSConfig
+                    )
+                    with mock.patch(
+                        "firebase_admin.messaging.send", return_value=True
+                    ) as send_mock:
+                        self.assertTrue(firebase_client.verify_token("xy"))
+                        send_mock.side_effect = UnregisteredError(
+                            "Token not registered"
+                        )
+                        self.assertFalse(firebase_client.verify_token("xy"))
+
+                    responses = [
+                        SendResponse({"name": "id-1"}, None),
+                        SendResponse(
+                            {"name": "id-2"}, UnregisteredError("Token not registered")
+                        ),
+                    ]
+                    batch_response = BatchResponse(responses)
+                    with mock.patch(
+                        "firebase_admin.messaging.send_multicast",
+                        return_value=batch_response,
+                    ):
+                        (
+                            success_count,
+                            failure_count,
+                            invalid_tokens,
+                        ) = firebase_client.send_message(
+                            ["token-1", "token-2"], {"tx-hash": "0x-random-hash"}
+                        )
+                        self.assertEqual(success_count, 1)
+                        self.assertEqual(failure_count, 1)
+                        self.assertEqual(invalid_tokens, ["token-2"])
+        finally:
+            FirebaseClientPool.firebase_client_pool.clear()
