86d529ef5963aa8f9868a03c2205603e883e1997
Uxio Fuentefria <uxio@gnosis.pm>
2021-11-17 17:18:08 +0100

    Support optimistic contracts
    - Related to https://github.com/gnosis/safe-deployments/pull/34

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -219,6 +219,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1227, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 1278, "1.3.0"),
     ],
+    EthereumNetwork.OPTIMISTIC: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 173749, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 173751, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -293,6 +297,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.OLYMPUS: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 1266),  # v1.3.0
+    ],
+    EthereumNetwork.OPTIMISTIC: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 173709),  # v1.3.0
     ],
 }
 
