9d847b3cf97bc763a0c2035398408ce251b90b3c
Uxio Fuentefria <uxio@gnosis.pm>
2021-11-24 13:29:53 +0100

    Add cache information for docker images
    - Currently caching of docker builds is not working on the CI
    - Add latest image tag

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -7,12 +7,14 @@ export DOCKER_BUILDKIT=1
 if [ "$1" = "develop" -o "$1" = "master" ]; then
     # If image does not exist, don't use cache
     docker pull gnosispm/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:$1 --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --build-arg BUILDKIT_INLINE_CACHE=1
 else
+    # Building tag version from staging image (vX.X.X)
     docker pull gnosispm/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging || \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from gnosispm/$DOCKERHUB_PROJECT:staging --build-arg BUILDKIT_INLINE_CACHE=1 || \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --build-arg BUILDKIT_INLINE_CACHE=1
+    docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:latest
 fi
 docker tag $DOCKERHUB_PROJECT gnosispm/$DOCKERHUB_PROJECT:$1
 docker push gnosispm/$DOCKERHUB_PROJECT:$1
