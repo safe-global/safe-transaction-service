1e92ecbf2cdc7f48acd315ed6c55f1d9ae44dbc2
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2022-01-06 17:45:34 +0100

    Disable IgnoreSucceededNone filter

--- safe_transaction_service/utils/tests/test_loggers.py
+++ safe_transaction_service/utils/tests/test_loggers.py
@@ -2,6 +2,8 @@ from logging import LogRecord
 
 from django.test import TestCase
 
+import pytest
+
 from ..loggers import IgnoreCheckUrl, IgnoreSucceededNone
 
 
@@ -21,6 +23,7 @@ class TestLoggers(TestCase):
         self.assertFalse(ignore_check_url.filter(check_log))
         self.assertTrue(ignore_check_url.filter(other_log))
 
+    @pytest.mark.skip(reason="Filter is disabled temporarily")  # TODO
     def test_ignore_succeeded_none(self):
         name = "name"
         level = 1
