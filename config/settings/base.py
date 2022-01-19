9532254a78b2db53a22d086da78d2930be8ea589
Uxio Fuentefria <uxio@gnosis.pm>
2022-01-19 10:48:34 +0100

    Enable CSRF
    - Closes #640

--- config/settings/base.py
+++ config/settings/base.py
@@ -104,7 +104,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.http.ConditionalGetMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
