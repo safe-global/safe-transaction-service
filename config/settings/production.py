019ccbf52b44b7c364e8c163270b9dbca79d1e26
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2022-01-19 19:29:58 +0100

    support for trusted csrf origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -60,6 +60,12 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
 X_FRAME_OPTIONS = "DENY"
+# https://docs.djangoproject.com/en/3.2/ref/settings/#csrf-trusted-origins
+allowed_csrf_origins = env("CSRF_TRUSTED_ORIGINS", default="")
+CSRF_TRUSTED_ORIGINS = [
+    allowed_csrf_origins.strip()
+    for allowed_csrf_origins in allowed_csrf_origins.split(",")
+]
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
