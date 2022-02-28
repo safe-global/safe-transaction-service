cdce54c1f64a38982e2a2e2a9c5f0e6b265e5fbe
Uxio Fuentefria <uxio@gnosis.pm>
2022-02-28 16:52:24 +0100

    Revert "Bump Django to v4 (#709)"
    This reverts commit 24b9b84c2e365948e64d1810bcc100004075aaa7.

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,8 +17,14 @@ DATABASES["default"]["ATOMIC_REQUESTS"] = False  # noqa F405
 # ------------------------------------------------------------------------------
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": env("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            # Mimicing memcache behavior.
+            # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
+            "IGNORE_EXCEPTIONS": True,
+        },
     }
 }
 
