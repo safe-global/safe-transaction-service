5bd9067594e202ccff5b4048f37a5195d4e56845
Uxio Fuentefria <uxio@gnosis.pm>
2022-03-15 17:30:39 +0100

    Fix typo: topt -> totp

--- safe_transaction_service/history/helpers.py
+++ safe_transaction_service/history/helpers.py
@@ -7,30 +7,32 @@ from eth_utils import keccak
 
 class DelegateSignatureHelper:
     @classmethod
-    def calculate_topt(
-        cls, topt_tx: int = 3600, topt_t0: int = 0, previous: bool = False
+    def calculate_totp(
+        cls, totp_tx: int = 3600, totp_t0: int = 0, previous: bool = False
     ) -> int:
         """
         https://en.wikipedia.org/wiki/Time-based_One-time_Password_algorithm
-        :param topt_tx:
-        :param topt_t0:
-        :param previous: Calculate topt for the previous interval
-        :return:
+
+        :param totp_tx: the Unix time from which to start counting time steps (default is 0)
+        :param totp_t0: an interval which will be used to calculate the value of the
+            counter CT (default is 3600 seconds).
+        :param previous: Calculate totp for the previous interval
+        :return: totp
         """
         if previous:
-            topt_t0 += topt_tx  # Allow previous interval
+            totp_t0 += totp_tx  # Allow previous interval
 
-        return int((time.time() - topt_t0) // topt_tx)
+        return int((time.time() - totp_t0) // totp_tx)
 
     @classmethod
     def calculate_hash(
         cls,
         address: ChecksumAddress,
         eth_sign: bool = False,
-        previous_topt: bool = False,
+        previous_totp: bool = False,
     ) -> bytes:
-        topt = cls.calculate_topt(previous=previous_topt)
-        message = address + str(topt)
+        totp = cls.calculate_totp(previous=previous_totp)
+        message = address + str(totp)
         if eth_sign:
             return keccak(
                 text="\x19Ethereum Signed Message:\n" + str(len(message)) + message
@@ -43,6 +45,6 @@ class DelegateSignatureHelper:
         return [
             cls.calculate_hash(delegate),
             cls.calculate_hash(delegate, eth_sign=True),
-            cls.calculate_hash(delegate, previous_topt=True),
-            cls.calculate_hash(delegate, eth_sign=True, previous_topt=True),
+            cls.calculate_hash(delegate, previous_totp=True),
+            cls.calculate_hash(delegate, eth_sign=True, previous_totp=True),
         ]
