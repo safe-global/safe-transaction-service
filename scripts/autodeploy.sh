8476547088ac9c224c1909ebeea851982f586a69
gonzalo.montiel <gonzalo.montiel@gnosis.pm>
2022-03-16 11:22:03 +0100

    Change name of env var TARGET_BRANCH to TARGET_ENV

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -6,5 +6,5 @@ curl -s --output /dev/null --write-out "%{http_code}" \
     -H "Content-Type: application/json" \
     -X POST \
     -u "$AUTODEPLOY_TOKEN" \
-    -d '{"push_data": {"tag": "'$TARGET_BRANCH'" }}' \
+    -d '{"push_data": {"tag": "'$TARGET_ENV'" }}' \
     $AUTODEPLOY_URL
