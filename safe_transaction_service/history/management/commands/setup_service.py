6a4da99fe73034b24881e8442944a79b918c4e4a
philipappiah <pappiahkubi1@gmail.com>
2022-05-19 08:39:21 +0100

    add meter network mainnet and testnet

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -264,6 +264,12 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 748810, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 748815, "1.3.0"),
     ],
+    EthereumNetwork.METER: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 23863901, "1.3.0+L2") # v1.3.0
+    ],
+    EthereumNetwork.METER_TESTNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 15035438, "1.3.0+L2") # v1.3.0
+    ]
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -370,6 +376,12 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.REI_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 748768),  # v1.3.0
     ],
+     EthereumNetwork.METER: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 23863720) # v1.3.0
+    ],
+    EthereumNetwork.METER_TESTNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 15035363) #v1.3.0
+    ]
 }
 
 
