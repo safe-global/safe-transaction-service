880a8bd3470be0943ac9e72899501f42623c06ba
Uxio Fuentefria <uxio@safe.global>
2022-05-23 17:43:40 +0200

    Add method to get token logos from coingecko

--- safe_transaction_service/tokens/clients/coingecko_client.py
+++ safe_transaction_service/tokens/clients/coingecko_client.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Optional
+from functools import lru_cache
+from typing import Any, Dict, Optional
 from urllib.parse import urljoin
 
 import requests
@@ -7,7 +8,11 @@ from eth_typing import ChecksumAddress
 
 from gnosis.eth import EthereumNetwork
 
-from safe_transaction_service.tokens.clients.exceptions import CannotGetPrice
+from safe_transaction_service.tokens.clients.exceptions import (
+    CannotGetPrice,
+    Coingecko404,
+    CoingeckoRequestError,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -47,22 +52,32 @@ class CoingeckoClient:
             EthereumNetwork.XDAI,
         )
 
-    def _get_price(self, url: str, name: str):
+    def _do_request(self, url: str) -> Dict[str, Any]:
         try:
             response = self.http_session.get(url, timeout=10)
             if not response.ok:
-                raise CannotGetPrice
-            # Result is returned with lowercased `token_address`
-            price = response.json().get(name)
+                if response.status_code == 404:
+                    raise Coingecko404
+                raise CoingeckoRequestError(url)
+            return response.json()
+        except (ValueError, IOError) as e:
+            logger.warning("Problem fetching %s", url)
+            raise CoingeckoRequestError from e
+
+    def _get_price(self, url: str, name: str):
+        try:
+            result = self._do_request(url)
+
+            # Result is returned with lowercased `name` (if querying by contract address, then `token_address`)
+            price = result.get(name)
             if price and price.get("usd"):
                 return price["usd"]
             else:
                 raise CannotGetPrice(f"Price from url={url} is {price}")
-        except (ValueError, IOError) as e:
-            logger.warning(
-                "Problem getting usd value on coingecko for token-name=%s", name
-            )
-            raise CannotGetPrice from e
+        except CoingeckoRequestError as e:
+            raise CannotGetPrice(
+                f"Cannot get price from Coingecko for token={name}"
+            ) from e
 
     def get_price(self, name: str) -> float:
         """
@@ -86,6 +101,25 @@ class CoingeckoClient:
             f"api/v3/simple/token_price/{self.asset_platform}?contract_addresses={token_address}&vs_currencies=usd",
         )
         return self._get_price(url, token_address)
+
+    @lru_cache(maxsize=128)
+    def get_token_info(
+        self, token_address: ChecksumAddress
+    ) -> Optional[Dict[str, Any]]:
+        token_address = token_address.lower()
+        url = urljoin(
+            self.base_url,
+            f"api/v3/coins/{self.asset_platform}/contract/{token_address}",
+        )
+        try:
+            return self._do_request(url)
+        except Coingecko404:
+            return None
+
+    def get_token_logo_url(self, token_address: ChecksumAddress) -> Optional[str]:
+        token_info = self.get_token_info(token_address)
+        if token_info:
+            return token_info["image"]["large"]
 
     def get_avax_usd_price(self) -> float:
         return self.get_price("avalanche-2")
