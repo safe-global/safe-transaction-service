ec7626f103bfdd6b057f0a768370d942c6ce2a52
Dimitris Lamprinos <pkakelas@gmail.com>
2022-08-13 18:12:51 +0300

    Add price calculation of milkomeda C1 tokens

--- safe_transaction_service/tokens/services/price_service.py
+++ safe_transaction_service/tokens/services/price_service.py
@@ -155,6 +155,9 @@ class PriceService:
     def get_aurora_usd_price(self) -> float:
         return self.coingecko_client.get_aoa_usd_price()
 
+    def get_cardano_usd_price(self) -> float:
+        return self.binance_client.get_ada_usd_price()
+
     def get_binance_usd_price(self) -> float:
         try:
             return self.binance_client.get_bnb_usd_price()
@@ -215,6 +218,11 @@ class PriceService:
             return self.coingecko_client.get_gather_usd_price()
         elif self.ethereum_network == EthereumNetwork.AVALANCHE:
             return self.get_avalanche_usd_price()
+        elif self.ethereum_network in (
+            EthereumNetwork.MILKOMEDA_C1_TESTNET,
+            EthereumNetwork.MILKOMEDA_C1_MAINNET,
+        ):
+            return self.get_cardano_usd_price()
         elif self.ethereum_network in (
             EthereumNetwork.AURORA,
             EthereumNetwork.AURORA_BETANET,
