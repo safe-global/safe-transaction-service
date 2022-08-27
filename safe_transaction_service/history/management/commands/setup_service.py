052d8a66d23e39ba94b5d4ead0df38ebeee254e4
Samuel Safahi <60489622+samisbakedham@users.noreply.github.com>
2022-08-27 16:00:10 -0400

    Add Candle

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -316,6 +316,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 1395140, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 3002760, "1.3.0"),
     ],
+     EthereumNetwork.CANDLE: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 2953008, "1.3.0+L2"),
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 2953013, "1.3.0"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -463,6 +467,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.CRONOS_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2958469),  # v1.3.0
+    ],
+    EthereumNetwork.CANDLE: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 2952983),  # v1.3.0
     ],
 }
 
