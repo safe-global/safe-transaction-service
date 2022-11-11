2ebefc40da07d4658e400a2738a67ca24f030ecb
Uxio Fuentefria <uxio@safe.global>
2022-11-11 16:32:18 +0100

    Remove unused class

--- safe_transaction_service/safe_messages/pagination.py
+++ safe_transaction_service/safe_messages/pagination.py
@@ -4,8 +4,3 @@ from rest_framework.pagination import LimitOffsetPagination
 class DefaultPagination(LimitOffsetPagination):
     max_limit = 200
     default_limit = 100
-
-
-class SmallPagination(LimitOffsetPagination):
-    max_limit = 100
-    default_limit = 20
