e8be712439247a80794ec7afa53d93c2bade309a
moisses89 <moisses89.fn@gmail.com>
2022-11-28 11:08:22 +0100

    Enable authentication in analytics endpoints

--- safe_transaction_service/analytics/views_v2.py
+++ safe_transaction_service/analytics/views_v2.py
@@ -1,4 +1,6 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
@@ -11,6 +13,8 @@ class AnalyticsMultisigTxsByOriginListView(ListAPIView):
     pagination_class = None
     swagger_schema = None
     renderer_classes = (JSONRenderer,)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, format=None):
         analytics_service = get_analytics_service()
