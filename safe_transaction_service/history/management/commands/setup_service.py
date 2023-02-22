5a7dc1e578d92473831531b2db7b78b23327fb3e
Uxio Fuentefria <uxio@safe.global>
2023-02-22 14:24:10 +0100

    Set version 4.15.2

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -380,7 +380,7 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     ],
     EthereumNetwork.BASE_GOERLI_TESTNET: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 938848, "1.3.0+L2"),
-        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 938848, "1.3.0"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 939064, "1.3.0"),
     ],
 }
 
