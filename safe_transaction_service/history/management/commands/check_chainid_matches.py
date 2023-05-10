4ef69092410178c5189d7d7ead967327e26681b5
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-05-10 13:10:12 +0200

    Support checking every chain
    - Use `get_chain_id` instead of `get_network` so networks not present on `EthereumNetwork` enum are supported
    - Closes #1459

--- safe_transaction_service/history/management/commands/check_chainid_matches.py
+++ safe_transaction_service/history/management/commands/check_chainid_matches.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from safe_transaction_service.utils.ethereum import get_ethereum_network
+from safe_transaction_service.utils.ethereum import get_chain_id
 
 from ...models import Chain
 
@@ -9,20 +9,18 @@ class Command(BaseCommand):
     help = "Check current connected Ethereum RPC chainId matches the one previously configured"
 
     def handle(self, *args, **options):
-        ethereum_network = get_ethereum_network()
+        chain_id = get_chain_id()
 
         try:
             chain = Chain.objects.get()  # Only one element in the table
         except Chain.DoesNotExist:
-            chain = Chain.objects.create(chain_id=ethereum_network.value)
+            chain = Chain.objects.create(chain_id=chain_id)
 
-        if chain.chain_id == ethereum_network.value:
+        if chain.chain_id == chain_id:
             self.stdout.write(
-                self.style.SUCCESS(
-                    f"EthereumRPC chainId {ethereum_network.value} looks good"
-                )
+                self.style.SUCCESS(f"EthereumRPC chainId {chain_id} looks good")
             )
         else:
             raise CommandError(
-                f"EthereumRPC chainId {ethereum_network.value} does not match previously used chainId {chain.chain_id}"
+                f"EthereumRPC chainId {chain_id} does not match previously used chainId {chain.chain_id}"
             )
