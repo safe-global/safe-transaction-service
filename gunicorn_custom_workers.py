a38e4787dbe97f02dbe965ccf5d455e054c416e4
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-07-21 13:26:09 +0200

    Make timeouts work in gunicorn
    - Closes #439
    - Refactor gunicorn configuration to be set via envvar

---
+++ gunicorn_custom_workers.py
@@ -0,0 +1,11 @@
+import gevent
+from gunicorn.workers.ggevent import GeventWorker
+
+
+class MyGeventWorker(GeventWorker):
+    def handle_request(self, listener_name, req, sock, addr):
+        try:
+            with gevent.Timeout(self.cfg.timeout):
+                super().handle_request(listener_name, req, sock, addr)
+        except gevent.Timeout:
+            self.log.error("TimeoutError on %s", req.path)
