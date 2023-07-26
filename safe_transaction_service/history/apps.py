59d599acf201ec50b38f7a5288824be2d2164c53
Uxio Fuentefria <uxio0@users.noreply.github.com>
2023-07-26 11:37:13 +0200

    Preload tx decoder when app starts
    - Tx decoder could take a few minutes to be loaded
    - Before it was done by the first query that reached the tx service
    - If there were multiple concurrent queries, tx service will be doing the computation multiple times, even if just one will be used for the cache
    - Add logging to `get_redis` to find out if the same could be happening

--- safe_transaction_service/history/apps.py
+++ safe_transaction_service/history/apps.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 
 
@@ -7,3 +9,14 @@ class HistoryConfig(AppConfig):
 
     def ready(self):
         from . import signals  # noqa
+
+        for argument in sys.argv:
+            if "gunicorn" in argument:  # pragma: no cover
+                # Just run this on production
+                # TODO Find a better way
+                from safe_transaction_service.contracts.tx_decoder import (
+                    get_db_tx_decoder,
+                )
+
+                get_db_tx_decoder()  # Build tx decoder cache
+                break
--- safe_transaction_service/utils/redis.py
+++ safe_transaction_service/utils/redis.py
@@ -1,10 +1,14 @@
+import logging
 from functools import cache
 
 from django.conf import settings
 
 from redis import Redis
 
+logger = logging.getLogger(__name__)
+
 
 @cache
 def get_redis() -> Redis:
+    logger.info("Opening connection to Redis")
     return Redis.from_url(settings.REDIS_URL)
