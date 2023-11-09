95c3fe318079ef03bef304d5b067e610313bd655
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-11-09 18:25:07 +0100

    Fix admin search slow
    - Implement new AdvancedAdminSearchMixin
    - Don't convert every value to text for search, so DB indexes can be used
    - Change admin search from `iexact (=)`  to `exact (==)`

--- safe_transaction_service/safe_messages/admin.py
+++ safe_transaction_service/safe_messages/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 
-from gnosis.eth.django.admin import BinarySearchAdmin
+from safe_transaction_service.utils.admin import AdvancedAdminSearchMixin
 
 from .models import SafeMessage, SafeMessageConfirmation
 
 
 @admin.register(SafeMessage)
-class SafeMessageAdmin(BinarySearchAdmin):
+class SafeMessageAdmin(AdvancedAdminSearchMixin, admin.ModelAdmin):
     date_hierarchy = "created"
     list_display = ("safe", "message_hash", "proposed_by", "message")
     ordering = ["-created"]
     readonly_fields = ("message_hash",)
-    search_fields = ["=safe", "=message_hash", "=proposed_by", "message"]
+    search_fields = ["==safe", "==message_hash", "==proposed_by", "message"]
 
 
 @admin.register(SafeMessageConfirmation)
-class SafeMessageConfirmationAdmin(BinarySearchAdmin):
+class SafeMessageConfirmationAdmin(AdvancedAdminSearchMixin, admin.ModelAdmin):
     date_hierarchy = "created"
     list_display = (
         "safe_message",
@@ -26,7 +26,6 @@ class SafeMessageConfirmationAdmin(BinarySearchAdmin):
     list_select_related = ("safe_message",)
     ordering = ["-created"]
     search_fields = [
-        "=safe_message__safe",
-        "=owner",
-        "safe_message__description",
+        "==safe_message__safe",
+        "==owner",
     ]
