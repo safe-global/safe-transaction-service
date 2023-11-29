fe4879dd7c254dfba8cf9a3c4c48b4bda4d8de73
Peter <peter@thx.network>
2023-11-29 16:51:05 +0100

    Adds management command for inserting safe master copy

---
+++ safe_transaction_service/history/management/commands/insert_safe_master_copy.py
@@ -0,0 +1,48 @@
+import binascii
+
+from django.core.management.base import BaseCommand
+from ...models import SafeL2MasterCopy
+
+class Command(BaseCommand):
+    help = "Insert SafeL2MasterCopy objects"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--address", help="SafeMasterCopy address", required=True)
+        parser.add_argument("--initial-block-number", help="Initial block number", required=False, default=0)
+        parser.add_argument("--tx-block-number", help="Transaction block number", required=False, default=None)
+        parser.add_argument("--version", help="Safe Version", required=False, default="1.3.0")
+        parser.add_argument("--l2", help="Address on L2", required=False, default=True)
+        parser.add_argument("--deployer", help="Deployer", required=False, default="Gnosis")
+
+    def handle(self, *args, **options):
+        mastercopy_address = options["address"]
+        initial_block_number = options["initial-block-number"]
+        tx_block_number = options["tx-block-number"]
+        version = options["version"]
+        l2 = options["l2"]
+        deployer = options["deployer"]
+
+        # Convert the mastercopy address to binary
+        address = self.ethereum_address_to_binary(mastercopy_address)
+        
+        # Create SafeL2MasterCopy object
+        SafeL2MasterCopy.objects.create(
+            address=address,
+            initial_block_number=initial_block_number,
+            tx_block_number=tx_block_number,
+            version=version,
+            l2=l2,
+            deployer=deployer,
+        )
+
+        self.stdout.write(self.style.SUCCESS(f"Created SafeL2MasterCopy for {mastercopy_address}"))
+
+    def ethereum_address_to_binary(self, address):
+        # Remove the '0x' prefix from the address if it exists
+        if address.startswith('0x'):
+            address = address[2:]
+
+        # Convert the hexadecimal string to binary representation
+        binary_representation = binascii.unhexlify(address)
+
+        return binary_representation
