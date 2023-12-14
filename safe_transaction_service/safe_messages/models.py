3d022331ca4c29fa84f3405c19ba6c90de57e84e
Uxío <Uxio0@users.noreply.github.com>
2023-12-14 19:03:35 +0100

    Fix 1271 signature validation for messages (#1800)
    - Fix 1271 signature validation for messages
    - Refactor signature validation to prevent code duplication
    - Fix tests

--- safe_transaction_service/safe_messages/models.py
+++ safe_transaction_service/safe_messages/models.py
@@ -11,6 +11,8 @@ from gnosis.safe.safe_signature import SafeSignatureType
 
 logger = getLogger(__name__)
 
+SIGNATURE_LENGTH = 5_000
+
 
 class SafeMessage(TimeStampedModel):
     """
@@ -61,7 +63,7 @@ class SafeMessageConfirmation(TimeStampedModel):
         related_name="confirmations",
     )
     owner = EthereumAddressV2Field(db_index=True)
-    signature = HexField(max_length=5000)
+    signature = HexField(max_length=SIGNATURE_LENGTH)
     signature_type = models.PositiveSmallIntegerField(
         choices=[(tag.value, tag.name) for tag in SafeSignatureType], db_index=True
     )
