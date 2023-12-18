287ef7c5eb23d8366a3657669db32f79f025c852
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-12-18 16:19:57 +0100

    Fix 1271 signatures for messages
    - Closes #1726

--- safe_transaction_service/safe_messages/utils.py
+++ safe_transaction_service/safe_messages/utils.py
@@ -8,13 +8,16 @@ from gnosis.eth.eip712 import eip712_encode_hash
 from gnosis.safe import Safe
 
 
-def get_safe_message_hash_for_message(
-    safe_address: ChecksumAddress, message: str | Dict[str, Any]
-) -> Hash32:
-    message_hash: Hash32 = (
+def get_hash_for_message(message: str | Dict[str, Any]) -> Hash32:
+    return (
         defunct_hash_message(text=message)
         if isinstance(message, str)
         else eip712_encode_hash(message)
     )
+
+
+def get_safe_message_hash_for_message(
+    safe_address: ChecksumAddress, message_hash: Hash32
+) -> Hash32:
     safe = Safe(safe_address, EthereumClientProvider())
     return safe.get_message_hash(message_hash)
