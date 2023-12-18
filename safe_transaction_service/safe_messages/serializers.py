3cbe7a78a2f9355631d637464f854e9839cfd205
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-12-18 16:19:57 +0100

    Fix 1271 signatures for messages
    - Closes #1726

--- safe_transaction_service/safe_messages/serializers.py
+++ safe_transaction_service/safe_messages/serializers.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Optional, Sequence, Tuple, Union
 
-from eth_typing import ChecksumAddress, HexStr
+from eth_typing import ChecksumAddress, Hash32, HexStr
 from hexbytes import HexBytes
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -13,7 +13,7 @@ from gnosis.safe.safe_signature import SafeSignature, SafeSignatureType
 from safe_transaction_service.utils.serializers import get_safe_owners
 
 from .models import SIGNATURE_LENGTH, SafeMessage, SafeMessageConfirmation
-from .utils import get_safe_message_hash_for_message
+from .utils import get_hash_for_message, get_safe_message_hash_for_message
 
 
 # Request serializers
@@ -22,11 +22,14 @@ class SafeMessageSignatureParserMixin:
         self,
         safe_signatures: Sequence[SafeSignature],
         safe_address: ChecksumAddress,
+        message_hash: Hash32,
         safe_message: Optional[SafeMessage],
     ) -> Tuple[ChecksumAddress, SafeSignatureType]:
         """
         :param safe_signatures:
         :param safe_address:
+        :param message_hash: Original hash of the message (not the one tied to the Safe)
+        :param safe_message: Safe message database object (if already created)
         :return:
         :raises ValidationError:
         """
@@ -37,6 +40,9 @@ class SafeMessageSignatureParserMixin:
 
         ethereum_client = EthereumClientProvider()
         for safe_signature in safe_signatures:
+            if safe_signature.signature_type == SafeSignatureType.CONTRACT_SIGNATURE:
+                # For contract signatures it needs the preimage, not the SafeMessageHash
+                safe_signature.safe_tx_hash = message_hash
             if not safe_signature.is_valid(ethereum_client, safe_address):
                 raise ValidationError(
                     f"Signature={safe_signature.signature.hex()} for owner={safe_signature.owner} is not valid"
@@ -86,7 +92,10 @@ class SafeMessageSerializer(SafeMessageSignatureParserMixin, serializers.Seriali
         message = attrs["message"]
         signature = attrs["signature"]
         attrs["safe"] = safe_address
-        safe_message_hash = get_safe_message_hash_for_message(safe_address, message)
+        message_hash = get_hash_for_message(message)
+        safe_message_hash = get_safe_message_hash_for_message(
+            safe_address, message_hash
+        )
         attrs["message_hash"] = safe_message_hash
 
         if SafeMessage.objects.filter(message_hash=safe_message_hash).exists():
@@ -96,7 +105,7 @@ class SafeMessageSerializer(SafeMessageSignatureParserMixin, serializers.Seriali
 
         safe_signatures = SafeSignature.parse_signature(signature, safe_message_hash)
         owner, signature_type = self.get_valid_owner_from_signatures(
-            safe_signatures, safe_address, None
+            safe_signatures, safe_address, message_hash, None
         )
 
         attrs["proposed_by"] = owner
@@ -131,11 +140,12 @@ class SafeMessageSignatureSerializer(
         attrs["safe_message"] = safe_message
         signature: HexStr = attrs["signature"]
         safe_address = safe_message.safe
+        message_hash = get_hash_for_message(safe_message.message)
         safe_message_hash = safe_message.message_hash
 
         safe_signatures = SafeSignature.parse_signature(signature, safe_message_hash)
         owner, signature_type = self.get_valid_owner_from_signatures(
-            safe_signatures, safe_address, safe_message
+            safe_signatures, safe_address, message_hash, safe_message
         )
 
         attrs["owner"] = owner
