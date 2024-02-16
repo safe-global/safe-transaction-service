ab3e5e824d1da578d8713066d81b8598b22fe483
moisses89 <7888669+moisses89@users.noreply.github.com>
2024-02-16 14:24:33 +0100

    Add Print SQL limit configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -551,3 +551,7 @@ SWAGGER_SETTINGS = {
         "api_key": {"type": "apiKey", "in": "header", "name": "Authorization"}
     },
 }
+
+# Shell Plus
+# ------------------------------------------------------------------------------
+SHELL_PLUS_PRINT_SQL_TRUNCATE = env.int("SHELL_PLUS_PRINT_SQL_TRUNCATE", default=10_000)
