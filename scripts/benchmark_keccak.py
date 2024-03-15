b764d8ca96ec4b19d03c29e4b08e12b86bb8b8e3
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-15 14:17:11 +0100

    Add more iterations for keccak benchmark script

--- scripts/benchmark_keccak.py
+++ scripts/benchmark_keccak.py
@@ -6,32 +6,33 @@ from eth_hash.auto import keccak as eth_hash_keccak
 from web3 import Web3
 
 
-def eth_hash_benchmark():
+def eth_hash_benchmark() -> str:
     return eth_hash_keccak(os.urandom(32)).hex()
 
 
-def web3_benchmark():
+def web3_benchmark() -> str:
     return Web3.keccak(os.urandom(32)).hex()
 
 
-def cryptodome_benchmark():
-    k = crypto_keccak.new(data=os.urandom(32), digest_bits=256)
-    return k.hexdigest()
+def cryptodome_benchmark() -> str:
+    return crypto_keccak.new(data=os.urandom(32), digest_bits=256).hexdigest()
 
 
-def pysha3_benchmark():
+def pysha3_benchmark() -> str:
     return sha3.keccak_256(os.urandom(32)).hexdigest()
 
 
 if __name__ == "__main__":
     import timeit
 
+    iterations = 1_000_000
+
     print(
         "eth_hash",
         timeit.timeit(
             "eth_hash_benchmark()",
             setup="from __main__ import eth_hash_benchmark",
-            number=500000,
+            number=iterations,
             globals=globals(),
         ),
     )
@@ -40,7 +41,7 @@ if __name__ == "__main__":
         timeit.timeit(
             "web3_benchmark()",
             setup="from __main__ import web3_benchmark",
-            number=500000,
+            number=iterations,
             globals=globals(),
         ),
     )
@@ -49,16 +50,16 @@ if __name__ == "__main__":
         timeit.timeit(
             "cryptodome_benchmark()",
             setup="from __main__ import cryptodome_benchmark",
-            number=500000,
+            number=iterations,
             globals=globals(),
         ),
     )
     print(
-        "pysha3",
+        "safe-pysha3",
         timeit.timeit(
             "pysha3_benchmark()",
             setup="from __main__ import pysha3_benchmark",
-            number=500000,
+            number=iterations,
             globals=globals(),
         ),
     )
