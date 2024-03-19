183dc5eaca3512469aa3de0afdc65a8e861d78eb
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-19 17:37:51 +0100

    Update description

--- safe_transaction_service/account_abstraction/apps.py
+++ safe_transaction_service/account_abstraction/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AccountAbstractionConfig(AppConfig):
     name = "safe_transaction_service.account_abstraction"
-    verbose_name = "Account Abstraction (ERC4337) support for Safe Transaction Service"
+    verbose_name = "Account Abstraction (ERC4337) indexing support"
