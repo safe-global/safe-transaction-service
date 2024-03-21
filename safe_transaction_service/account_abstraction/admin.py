943e5b045831d716dd11cd3155bd11f18be0f323
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-21 16:39:54 +0100

    Add missing admin classes

--- safe_transaction_service/account_abstraction/admin.py
+++ safe_transaction_service/account_abstraction/admin.py
@@ -1,15 +1,49 @@
 from django.contrib import admin
 
-from .models import UserOperation
+from .models import SafeOperation, UserOperation, UserOperationReceipt
 
 
 @admin.register(UserOperation)
 class UserOperationAdmin(admin.ModelAdmin):
+    list_display = ("hash", "ethereum_tx_id", "sender", "nonce", "receipt__success")
+    list_filter = [
+        "receipt__success",
+    ]
+    search_fields = [
+        "==sender",
+    ]
+    ordering = ["-nonce"]
+
+
+@admin.register(UserOperationReceipt)
+class UserOperationReceiptAdmin(admin.ModelAdmin):
     list_display = (
-        "ethereum_tx_id",
-        "sender",
-        "nonce",
+        "user_operation__hash",
+        "user_operation__sender",
+        "user_operation__nonce",
+        "success",
+        "deposited",
     )
+    list_filter = [
+        "success",
+    ]
+    search_fields = [
+        "==sender",
+    ]
+    ordering = ["-nonce"]
+
+
+@admin.register(SafeOperation)
+class SafeOperationAdmin(admin.ModelAdmin):
+    list_display = (
+        "hash",
+        "user_operation__hash",
+        "ethereum_tx_id",
+        "user_operation__sender",
+        "user_operation__nonce",
+        "module_address",
+    )
+    list_filter = ["module_address"]
     search_fields = [
         "==sender",
     ]
