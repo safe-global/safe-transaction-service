b846df7ec47073698802ed21e078902e7ee74b4c
Uxio Fuentefria <uxio0@users.noreply.github.com>
2024-04-05 14:35:40 +0200

    Don't require 4337 Bundler RPC for tests
    - Use mocks instead

--- safe_transaction_service/account_abstraction/tests/services/test_aa_processor_service.py
+++ safe_transaction_service/account_abstraction/tests/services/test_aa_processor_service.py
@@ -4,6 +4,16 @@ from unittest.mock import MagicMock
 from django.test import TestCase
 
 from gnosis.eth import EthereumClient
+from gnosis.eth.account_abstraction import BundlerClient
+from gnosis.eth.account_abstraction import UserOperation as UserOperationClass
+from gnosis.eth.account_abstraction import (
+    UserOperationReceipt as UserOperationReceiptClass,
+)
+from gnosis.eth.tests.mocks.mock_bundler import (
+    safe_4337_user_operation_hash_mock,
+    user_operation_mock,
+    user_operation_receipt_mock,
+)
 
 from safe_transaction_service.account_abstraction.services import (
     get_aa_processor_service,
@@ -29,19 +39,42 @@ class TestAaProcessorService(TestCase):
     def setUp(self):
         super().setUp()
         get_aa_processor_service.cache_clear()
-        self.aa_processor_service = get_aa_processor_service()
+        with self.settings(ETHEREUM_4337_BUNDLER_URL="https://localhost"):
+            # Bundler must be defined so it's initialized and it can be mocked
+            self.aa_processor_service = get_aa_processor_service()
 
     def tearDown(self):
         super().tearDown()
         get_aa_processor_service.cache_clear()
 
     @mock.patch.object(
+        BundlerClient,
+        "get_user_operation_receipt",
+        autospec=True,
+        return_value=UserOperationReceiptClass.from_bundler_response(
+            user_operation_receipt_mock["result"]
+        ),
+    )
+    @mock.patch.object(
+        BundlerClient,
+        "get_user_operation_by_hash",
+        autospec=True,
+        return_value=UserOperationClass.from_bundler_response(
+            safe_4337_user_operation_hash_mock.hex(), user_operation_mock["result"]
+        ),
+    )
+    @mock.patch.object(
         EthereumClient,
         "get_chain_id",
         autospec=True,
         return_value=aa_chain_id,  # Needed for hashes to match
     )
-    def test_process_aa_transaction(self, get_chain_id_mock: MagicMock):
+    def test_process_aa_transaction(
+        self,
+        get_chain_id_mock: MagicMock,
+        get_user_operation_by_hash_mock: MagicMock,
+        get_user_operation_receipt_mock: MagicMock,
+    ):
         ethereum_tx = history_factories.EthereumTxFactory(
             logs=[clean_receipt_log(log) for log in aa_tx_receipt_mock["logs"]]
         )
