f80d751568d1f8dd72d183b93282656d21691f0e
moisses89 <7888669+moisses89@users.noreply.github.com>
2024-09-17 10:51:35 +0200

    Add server port

--- config/settings/local.py
+++ config/settings/local.py
@@ -55,3 +55,5 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+USE_X_FORWARDED_HOST = True
