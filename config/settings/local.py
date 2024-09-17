191bfc5dfd9891c3c04a29093cbd776331f95fa5
Moisés <7888669+moisses89@users.noreply.github.com>
2024-09-17 13:23:06 +0200

    Fix swagger locally is not working (#2208)

--- config/settings/local.py
+++ config/settings/local.py
@@ -55,3 +55,6 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+# Enable to get the host from header
+USE_X_FORWARDED_HOST = True
