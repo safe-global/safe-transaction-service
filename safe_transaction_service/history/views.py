d635eacefec43c79c65e79c28cef73547b5298e6
Felipe Alvarado <felipe@safe.global>
2024-10-30 13:20:44 +0100

    Apply PR suggestion
    Co-authored-by: Uxío <Uxio0@users.noreply.github.com>

--- safe_transaction_service/history/views.py
+++ safe_transaction_service/history/views.py
@@ -543,7 +543,7 @@ class SafeMultisigTransactionDetailView(RetrieveAPIView):
     def delete(self, request, safe_tx_hash: HexStr):
         """
         Removes the queued but not executed multi-signature transaction associated with the given Safe tansaction hash.
-        Only the proposer or the delegate user who proposed the transaction can delete it.
+        Only the proposer or the delegate who proposed the transaction can delete it.
         If the transaction was proposed by a delegate, it must still be a valid delegate for the transaction proposer.
         An EOA is required to sign the following EIP-712 data:
 
