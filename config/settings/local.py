0497d380e6edc1979ac75632651ed3db4c207a65
Vini murafa <06.poems_races@icloud.com>
2025-01-03 23:35:28 +0100

    typo fix local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -29,7 +29,7 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicing memcache behavior.
+            # Mimicking memcache behavior.
             # http://niwinz.github.io/django-redis/latest/#_memcached_exceptions_behavior
             "IGNORE_EXCEPTIONS": True,
         },
