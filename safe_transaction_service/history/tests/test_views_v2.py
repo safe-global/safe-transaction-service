d3a7d368c1d8509f37d77eb9e79c9c3d7a53ba6a
Felipe Alvarado <felipe@safe.global>
2025-02-24 10:21:06 +0100

    Update test with signature validation

--- safe_transaction_service/history/tests/test_views_v2.py
+++ safe_transaction_service/history/tests/test_views_v2.py
@@ -990,7 +990,10 @@ class TestViewsV2(SafeTestCaseMixin, APITestCase):
             ContractQuerySet.cache_trusted_addresses_for_delegate_call.clear()
 
     def test_get_multisig_transactions_filters(self):
-        safe_address = Account.create().address
+        safe_owner_1 = Account.create()
+        safe = self.deploy_test_safe(owners=[safe_owner_1.address])
+        safe_address = safe.address
+
         response = self.client.get(
             reverse("v2:history:multisig-transactions", args=(safe_address,)),
             format="json",
@@ -1059,7 +1062,10 @@ class TestViewsV2(SafeTestCaseMixin, APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["results"]), 0)
 
-        MultisigConfirmationFactory(multisig_transaction=multisig_transaction)
+        MultisigConfirmationFactory(
+            multisig_transaction=multisig_transaction,
+            force_sign_with_account=safe_owner_1,
+        )
         response = self.client.get(
             reverse("v2:history:multisig-transactions", args=(safe_address,))
             + "?has_confirmations=True",
