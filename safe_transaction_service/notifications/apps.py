f6ab003730dba09361742f7e69c0ddaa4c0a401a
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2025-03-10 16:19:11 +0100

    Remove notifications
    - Closes #2126

--- safe_transaction_service/notifications/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotificationsConfig(AppConfig):
-    name = "safe_transaction_service.notifications"
-    verbose_name = "Notifications for Safe Transaction Service"
