0be16ab8b1f2c0d0f2d7e7921c5dc649b42a0e27
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2025-11-26 12:06:43 +0100

    Fix collectibles cache key collision

--- safe_transaction_service/history/services/collectibles_service.py
+++ safe_transaction_service/history/services/collectibles_service.py
@@ -351,7 +351,10 @@ class CollectiblesService:
             # No need for further DB/Cache calls
             return [], 0
 
-        cache_key = f"collectibles:{safe_address}:{only_trusted}:{exclude_spam}:{limit}{offset}:{number_erc721_events}"
+        cache_key = (
+            f"collectibles:{safe_address}:{only_trusted}:{exclude_spam}:{limit}:{offset}:"
+            f"{number_erc721_events}"
+        )
         cache_key_count = (
             f"collectibles_count:{safe_address}:{only_trusted}:{exclude_spam}"
         )
