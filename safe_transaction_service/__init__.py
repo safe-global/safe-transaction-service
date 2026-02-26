699858b5ea657b233df7b3a1af5b68f913182c41
moisses89 <7888669+moisses89@users.noreply.github.com>
2026-02-26 13:41:26 +0100

    Set version to v6.0.3

--- safe_transaction_service/__init__.py
+++ safe_transaction_service/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: FSL-1.1-MIT
-__version__ = "6.0.2"
+__version__ = "6.0.3"
 __version_info__ = tuple(
     int(num) if num.isdigit() else num
     for num in __version__.replace("-", ".", 1).split(".")
