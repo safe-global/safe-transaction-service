8c48a01f03f7f5a8378a8dcb6bf265abd19297ae
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2026-04-27 16:19:46 +0200

    (fix) Celery DB pool cleanup

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,1 @@
 # SPDX-License-Identifier: FSL-1.1-MIT
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery_app import app as celery_app
-
-__all__ = ("celery_app",)
